f2ae5858a8c49a464188a2c5bd3c4f3194d8e3b7
Karlisson Bezerra <kmbezerra@uolinc.com>
2018-05-06 21:56:04 -0300

    refactor

--- heightmap/mapper.js
+++ heightmap/mapper.js
@@ -12,11 +12,6 @@ var ctx = canvas.getContext("2d");
 
 var log = console.log.bind(console);
 
-var LEFT = 37;
-var RIGHT = 39;
-var UP = 38;
-var DOWN = 40;
-
 var TILESIZE = 2;
 var GRID_WIDTH = 257;
 var GRID_HEIGHT = 257;
@@ -26,153 +21,136 @@ var WATERLEVEL = 45,
     MAX_HEIGHT = 100,
     ROUGHNESS = 1;
 
-var Tile = {
-    x: 0,
-    y: 0,
-    value: 0
-};
 
-var Grid = (function(w, h){
+var grid = (function(w, h){
     var _grid = [];
 
     for(var i = 0; i < h; i++){
         _grid.push([]);
         for(var j = 0; j < w; j++){
-            var tile = Object.clone(Tile);
-            tile.x = j;
-            tile.y = i;
-            tile.value = 0;
-            _grid[i].push(tile);
+            _grid[i].push(0);
         }
     }
 
     return {
         width: w,
         height: h,
-        get: function(x, y){
+        get: function(point){
+            var x = point.x,
+                y = point.y;
             if (y < 0 || y >= h || x < 0 || x >= w){
-                return {value: undefined};
+                return undefined;
             }
             return _grid[y][x];
         },
-        set: function(x, y, val){
+        set: function(point, val){
+            var x = point.x,
+                y = point.y;
             if (val > MAX_HEIGHT){
                 val = MAX_HEIGHT;
             }
             if (val < MIN_HEIGHT){
                 val = MIN_HEIGHT;
             }
-            _grid[y][x].value = val;
-        },
-        print: function(){
-            for(var i = 0; i < h; i++){
-                log(_grid[i].join(','));
-            }
+            _grid[y][x] = val;
         }
     };
 })(GRID_WIDTH, GRID_HEIGHT);
 
-canvas.width = GRID_WIDTH * TILESIZE;
-canvas.height = GRID_HEIGHT * TILESIZE;
-
-
-var Rand = {
-    int: function(num){
-        return parseInt(Math.random() * num, 10);
-    },
-    choice: function(min, max){
-        if (max < min){
-            return;
-        }
-        var arr = [];
-        for(var i=min; i<=max; i++){
-            arr.push(i);
-        }
-        return arr[this.int(arr.length)];
-    }
-};
 
 var Terrain = (function(){
     return {
         average: function(values) {
             var valid = values.filter(function(val) { return Boolean(val); });
-            var total = valid.reduce(function(sum, val) { return sum + val; }, 0);
+            var total = valid.reduce(function(sum, val) {
+                return sum + val;
+            }, 0);
+
             return Math.round(total / valid.length);
         },
-        diamond: function(grid, x, y, size, offset){
+        diamond: function(grid, point, size, offset){
+            var x = point.x,
+                y = point.y;
             var value = this.average([
-                grid.get(x, y - size).value,      // top
-                grid.get(x + size, y).value,      // right
-                grid.get(x, y + size).value,      // bottom
-                grid.get(x - size, y).value       // left
+                grid.get(Point.new(x, y - size)),      // top
+                grid.get(Point.new(x + size, y)),      // right
+                grid.get(Point.new(x, y + size)),      // bottom
+                grid.get(Point.new(x - size, y))       // left
             ]);
-            grid.set(x, y, value + offset);
+            grid.set(point, value + offset);
         },
-        square: function(grid, x, y, size, offset){
+        square: function(grid, point, size, offset){
+            var x = point.x,
+                y = point.y;
             var value = this.average([
-                grid.get(x - size, y - size).value,   // upper left
-                grid.get(x + size, y - size).value,   // upper right
-                grid.get(x + size, y + size).value,   // lower right
-                grid.get(x - size, y + size).value    // lower left
+                grid.get(Point.new(x - size, y - size)),   // upper left
+                grid.get(Point.new(x + size, y - size)),   // upper right
+                grid.get(Point.new(x + size, y + size)),   // lower right
+                grid.get(Point.new(x - size, y + size))    // lower left
             ]);
-            grid.set(x, y, value + offset);
+            grid.set(point, value + offset);
         },
-        diamondSquare: function(grid, nsize){
-            for(var size = nsize; size/2 >= 1; size /= 2){
+        diamondSquare: function(grid){
+            for(var size = GRID_WIDTH - 1; size/2 >= 1; size /= 2){
                 var half = size / 2,
                     scale = ROUGHNESS * size;
 
                 for (var y = half; y < GRID_WIDTH-1; y += size) {
                     for (var x = half; x < GRID_WIDTH-1; x += size) {
-                        this.square(grid, x, y, half, Rand.choice(-scale, scale));
+                        var variance = Random.int(-scale, scale);
+                        this.square(grid, Point.new(x, y), half, variance);
                     }
                 }
                 for (var y = 0; y <= GRID_WIDTH-1; y += half) {
                     for (var x = (y + half) % size; x <= GRID_WIDTH-1; x += size) {
-                        this.diamond(grid, x, y, half, Rand.choice(-scale, scale));
+                        var variance = Random.int(-scale, scale);
+                        this.diamond(grid, Point.new(x, y), half, variance);
                     }
                 }
             }
         },
         generate: function(grid){
-            grid.set(0, 0, MAX_HEIGHT);
-            grid.set(GRID_WIDTH-1, 0, MAX_HEIGHT);
-            grid.set(0, GRID_HEIGHT-1, MAX_HEIGHT);
-            grid.set(GRID_WIDTH-1, GRID_HEIGHT-1, MAX_HEIGHT);
+            var randInt = function() {
+                return Random.int(MIN_HEIGHT, MAX_HEIGHT);
+            };
+            grid.set(Point.new(0, 0), randInt());
+            grid.set(Point.new(GRID_WIDTH-1, 0), randInt());
+            grid.set(Point.new(0, GRID_HEIGHT-1), randInt());
+            grid.set(Point.new(GRID_WIDTH-1, GRID_HEIGHT-1), randInt());
 
-            this.diamondSquare(grid, GRID_WIDTH - 1);
+            this.diamondSquare(grid);
         },
     };
 })();
 
 
-var draw = function(){
+var draw = function(grid){
     //desenha o cenario
-    for(var i = 0; i < GRID_WIDTH; i++){
-        for(var j = 0; j < GRID_HEIGHT; j++){
+    for(var i = 0; i < grid.width; i++){
+        for(var j = 0; j < grid.height; j++){
             ctx.beginPath();
             ctx.fillStyle = "ForestGreen";
-            var tile = Grid.get(i, j).value;
-            if (tile < WATERLEVEL){
+            var value = grid.get(Point.new(i, j));
+            if (value < WATERLEVEL){
                 ctx.fillStyle = "darkblue";
             }
 
-            if (tile >= WATERLEVEL && tile <= WATERLEVEL+10){
+            if (value >= WATERLEVEL && value <= WATERLEVEL+10){
                 ctx.fillStyle = "blue";
             }
 
-            if (tile > WATERLEVEL+10 && tile <= WATERLEVEL+15){
+            if (value > WATERLEVEL+10 && value <= WATERLEVEL+15){
                 ctx.fillStyle = "#E1D595";  //beach
             }
 
-            if (tile > WATERLEVEL+15){
+            if (value > WATERLEVEL+15){
                 ctx.fillStyle = "darkgreen";
             }
 
-            if (tile >= MAX_HEIGHT-10){
+            if (value >= MAX_HEIGHT-10){
                 ctx.fillStyle = "gray";
             }
-            if (tile >= MAX_HEIGHT-5){
+            if (value >= MAX_HEIGHT-5){
                 ctx.fillStyle = "#DDD";
             }
 
@@ -182,6 +160,9 @@ var draw = function(){
     }
 };
 
-Terrain.generate(Grid);
+canvas.width = grid.width * TILESIZE;
+canvas.height = grid.height * TILESIZE;
 
-draw();
+Terrain.generate(grid);
+
+draw(grid);
