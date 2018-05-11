1b631e5c616f8bf096f82fc4a72cebac89b8eff9
Karlisson Bezerra <kmbezerra@uolinc.com>
2018-05-11 18:59:08 -0300

    Add point distance function

--- base.js
+++ base.js
@@ -106,6 +106,10 @@ var Point = (function(){
         add: function(p1, p2){
             return Point.new(p1.x + p2.x, p1.y + p2.y);
         },
+        distance: function(p1, p2){
+            var sum = Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2);
+            return Math.sqrt(sum);
+        },
         random: function(max_width, max_height){
             var x = Random.int(0, GRID_WIDTH),
                 y = Random.int(0, GRID_HEIGHT);
