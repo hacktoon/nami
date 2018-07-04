0674c604f2763b08ca605ea197a3a1df753c9d2f
Karlisson Bezerra <macedobz@gmail.com>
2018-07-03 22:29:34 -0300

    Update Plates code to use GridFill class

--- plates/plates.js
+++ plates/plates.js
@@ -12,10 +12,7 @@ var TOP = 1,
     NUM_PLATES = 10;
 
 var grid = Grid.new(128, 128),
-    plates = [],
-    scheduled = [],
-    totalPoints = grid.width * grid.height,
-    visitedPoints = 0;
+    plates = [];
 
 var platesColorMap = (function() {
     var colors = [];
@@ -25,60 +22,34 @@ var platesColorMap = (function() {
     return colors;
 })();
 
+var regionMap = {};
+
 var Plate = (function(){
 
-    var _Plate = function(id, startPoint){
+    var _Plate = function(id){
         this.id = id;
-        this.points = [startPoint];
-        this.edges = [startPoint];
-        this.scheduled = [];
-        this.speed = 5;
-        this.weight = 10;
+        this.points = [];
+        this.speed = _.sample([1, 2, 3, 4, 5]);
+        this.weight = _.sample([1, 2]);
         this.direction = _.sample([TOP, LEFT, RIGHT, BOTTOM]);
-
-        this.addPoint = function(point) {
-            this.points.push(point);
-        };
-
-        this.grow = function(grid) {
-            var newEdges = [],
-                self = this;
-
-            this.edges.forEach(function(point){
-                var edgeNeighbours = Point.neighborHood(point, 'axials');
-
-                edgeNeighbours.forEach(function(neighbour){
-                    if (_.sample([true, false])){
-                        grid.set(neighbour, self.id);
-                        visitedPoints++;
-                        newEdges.push(neighbour);
-                    } else {
-                        newEdges.push(point);
-                    }
-                });
-            });
-            this.scheduled = newEdges;
-        };
     };
 
     return {
         _class: _Plate,
-        new: function(id, startPoint){
-            return new _Plate(id, startPoint);
+        new: function(id){
+            return new _Plate(id);
         }
     };
 })();
 
-var createPlates = function(grid, numPoints) {
-    var points = grid.randomPoints(numPoints),
-        plates = [],
-        id = 0;
+var createPlates = function(grid, totalPlates) {
+    var points = grid.randomPoints(totalPlates),
+        plates = [];
 
-    points.forEach(function(startPoint) {
-        var plate = Plate.new(id++, startPoint);
-        plate.scheduled.push(startPoint);
-        grid.set(startPoint, plate.id);
-        visitedPoints++;
+    _.times(totalPlates, function(i) {
+        var plate = Plate.new(i);
+
+        regionMap[i] = GridFill.new(grid, points[i], i);
         plates.push(plate);
     });
     return plates;
@@ -96,7 +67,6 @@ var draw = function(grid){
 };
 
 var reset = function() {
-    visitedPoints = 0;
     grid.reset();
     init();
 };
@@ -104,17 +74,13 @@ var reset = function() {
 var init = function() {
     plates = createPlates(grid, NUM_PLATES);
     plates.forEach(function(plate) {
-        plate.grow(grid);
+        regionMap[plate.id].grow();
     });
 };
 
 generateButton.addEventListener('click', function() {
     reset();
-    while(visitedPoints < totalPoints){
-        plates.forEach(function(plate) {
-            plate.grow(grid);
-        });
-    }
+
     draw(grid);
 });
 
@@ -125,7 +91,7 @@ resetButton.addEventListener('click', function() {
 
 growButton.addEventListener('click', function() {
     plates.forEach(function(plate) {
-        plate.grow(grid);
+        regionMap[plate.id].grow();
     });
     draw(grid);
 });
