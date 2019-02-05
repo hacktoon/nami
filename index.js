59d9686351b397704c62eeb4c007127d65bb846a
Karlisson Bezerra <macedobz@gmail.com>
2019-02-05 00:47:01 -0200

    Organize code

--- index.js
+++ index.js
@@ -28,30 +28,24 @@ var world = createWorld();
 
 generateButton.addEventListener('click', function() {
     world = createWorld();
-    drawMap(mapCtx, world.heightMap, {colorMap: heightmapColorMap});
     seaLevelInput.value = world.seaLevel;
-    log("land: " + world.data.land, "water: " + world.data.water);
-    if (world.data.land > world.data.water) {
-        log('more land');
-    } else {
-        log('more water');
-    }
-});
-
-surfaceViewInput.addEventListener('click', function(e) {
     drawMap(mapCtx, world.heightMap, {colorMap: heightmapColorMap});
 });
 
-moistureViewInput.addEventListener('click', function(e) {
+surfaceViewInput.addEventListener('click', function() {
+    drawMap(mapCtx, world.heightMap, {colorMap: heightmapColorMap});
+});
+
+moistureViewInput.addEventListener('click', function() {
     drawMap(mapCtx, world.moistureMap, {colorMap: moistureColorMap});
 });
 
-tectonicsViewInput.addEventListener('click', function(e) {
+tectonicsViewInput.addEventListener('click', function() {
     tectonicsPainter.draw();
     tectonicsPainter.drawEdges();
 });
 
-seaLevelInput.addEventListener('change', function(e) {
+seaLevelInput.addEventListener('change', function() {
     world.seaLevel = seaLevelInput.value;
     generateSurfaceColorMap();
     drawMap(mapCtx, world.heightMap, {colorMap: heightmapColorMap});
