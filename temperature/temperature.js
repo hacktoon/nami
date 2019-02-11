0c935c7b2e5d4c05fb8b5ce080bd77e78d9a4385
Karlisson Bezerra <macedobz@gmail.com>
2019-02-11 18:17:12 -0200

    Add variation to frequency

--- temperature/temperature.js
+++ temperature/temperature.js
@@ -6,29 +6,39 @@ var Temperature = (function(){
         this.zones = {
             1: {name: "Polar"},
             2: {name: "Temperate"},
-            3: {name: "Subtropic"},
-            4: {name: "Tropic"}
+            3: {name: "Subtropical"},
+            4: {name: "Tropical"}
         }
         this.points = [];
 
-        this.createZoneLine = function(yAxis, amplitude){
-            var x = _.random(1, 5);
+        this.createZoneLine = function(baseY, amplitude){
+            var baseX = _.random(1, 5);
 
-            var getFrequency = function(){
-                return _.random(.1, .5, true);
+            var buildPoint = function(x){
+                var y = baseY + calculateY(baseX);
+                baseX += getFrequency();
+                return Point.new(x, y);
             };
 
-            var calculateY = function(x){
+            var calculateY = function(x) {
+                // a superposition of different sine functions
                 var y = Math.sin(x) + Math.sin(x / 4) + Math.sin(x / 8);
                 return Math.floor(y * amplitude);
             };
 
+            var getFrequency = function(){
+                if (_.sample([true, false])) return 0;
+                return _.random(.2, .6, true);
+            };
+
             _.times(self.size, function(count){
-                var y = yAxis + calculateY(x);
-                    point = Point.new(count, y);
-                x += getFrequency();
+                var point = buildPoint(count);
                 self.points.push(point);
             });
+        };
+
+        this.createZone = function(startY, endY, zoneInfo){
+
         };
 
         this.build = function(){
@@ -36,6 +46,8 @@ var Temperature = (function(){
             self.createZoneLine(64, 1.5);
             self.createZoneLine(127, 2);
             self.createZoneLine(200, 3);
+
+            self.createZone(0, 32, self.zones[1]);
         };
     };
 
