fc13baa0564d6fe64db238b3e40f2a4f0a757cc3
Karlisson Bezerra <macedobz@gmail.com>
2019-03-19 23:40:45 -0300

    Add waterBody class

---
+++ src/world/water-body.js
@@ -0,0 +1,19 @@
+import _ from 'lodash'
+
+
+const waterBodyTable = [
+    { id: 0, type: "Lake", color: "#000056", scale: -3 }
+]
+
+
+export default class WaterBody {
+    constructor() {
+        this.
+    }
+
+    get id() { return this.waterBody.id }
+    get height() { return this.waterBody.height }
+    get color() { return this.waterBody.color }
+
+
+}
