6c7624c8b5d5a3977c19e0ec626b432c498b6ec8
Karlisson Bezerra <macedobz@gmail.com>
2019-03-28 20:39:23 -0300

    Name reliefs

--- src/world/geo/relief.js
+++ src/world/geo/relief.js
@@ -4,23 +4,21 @@ import { Grid } from '../../lib/grid';
 import { HeightMap } from '../../lib/heightmap'
 
 
-
-const RIVER_ELEVATION = 5
-const ELEVATION_TABLE = [
-    { id: 0, height: 0,   color: "#000056", value: 0 },
-    { id: 1, height: 80,  color: "#1a3792", value: 1 },
-    { id: 2, height: 120, color: "#3379a6", value: 2 },
-    { id: 3, height: 150, color: "#0a5816", value: 3 },
-    { id: 4, height: 190, color: "#31771a", value: 4 },
-    { id: 5, height: 240, color: "#6f942b", value: 5 },
-    { id: 6, height: 254, color: "#BBBBBB", value: 6 },
-    { id: 7, height: 257, color: "#EEEEEE", value: 7 }
+const RELIEF_TABLE = [
+    { id: 0, height: 0,   color: "#000056", value: 0, name: "Abyssal" },
+    { id: 1, height: 80,  color: "#1a3792", value: 1, name: "Pelagic" },
+    { id: 2, height: 120, color: "#3379a6", value: 2, name: "Shelf" },
+    { id: 3, height: 150, color: "#0a5816", value: 3, name: "Basin" },
+    { id: 4, height: 190, color: "#31771a", value: 4, name: "Plain" },
+    { id: 5, height: 240, color: "#6f942b", value: 5, name: "Hill" },
+    { id: 6, height: 254, color: "#BBBBBB", value: 6, name: "Mountain" },
+    { id: 7, height: 257, color: "#EEEEEE", value: 7, name: "Peak" }
 ]
 
 
 class Relief {
     constructor(id) {
-        this.data = ELEVATION_TABLE[id]
+        this.data = RELIEF_TABLE[id]
     }
 
     get id() { return this.data.id }
@@ -29,27 +27,27 @@ class Relief {
     get isBelowSeaLevel () { return this.data.value < 3 } // remove
     get isAboveSeaLevel() { return this.data.value >= 3 } // remove
     get isMiddle () {
-        let middle = Math.floor(ELEVATION_TABLE.length / 2)
+        let middle = Math.floor(RELIEF_TABLE.length / 2)
         return this.data.value == middle
     }
 
     raise (amount=1) {
         let newId = this.data.id + amount
-        let id = _.clamp(newId, 0, ELEVATION_TABLE.length-1)
-        this.data = ELEVATION_TABLE[id]
+        let id = _.clamp(newId, 0, RELIEF_TABLE.length-1)
+        this.data = RELIEF_TABLE[id]
     }
 
     lower (amount=1) {
         let newId = this.data.id - amount
-        let id = _.clamp(newId, 0, ELEVATION_TABLE.length-1)
-        this.data = ELEVATION_TABLE[id]
+        let id = _.clamp(newId, 0, RELIEF_TABLE.length-1)
+        this.data = RELIEF_TABLE[id]
     }
 
     level(newId) {
         let oldId = this.data.id
-        let id = _.clamp(newId, 0, ELEVATION_TABLE.length - 1)
+        let id = _.clamp(newId, 0, RELIEF_TABLE.length - 1)
         if (oldId > id) {
-            this.data = ELEVATION_TABLE[id]
+            this.data = RELIEF_TABLE[id]
         }
     }
 
@@ -62,15 +60,15 @@ class Relief {
     }
 
     get isLowest() {
-        return this.data.id == _.first(ELEVATION_TABLE).id
+        return this.data.id == _.first(RELIEF_TABLE).id
     }
 
     get isHighest() {
-        return this.data.id == _.last(ELEVATION_TABLE).id
+        return this.data.id == _.last(RELIEF_TABLE).id
     }
 
     get isRiverPossible() {
-        return this.data.id == RIVER_ELEVATION
+        return this.data.id == 5
     }
 }
 
@@ -100,7 +98,7 @@ export class ReliefMap {
 
     getReliefId(height) {
         let id = 0
-        for (let reliefData of ELEVATION_TABLE) {
+        for (let reliefData of RELIEF_TABLE) {
             if (height >= reliefData.height) {
                 id = reliefData.id
             } else {
