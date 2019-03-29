194de577b6475277e5eb28a4292c14a603e9225f
Karlisson Bezerra <macedobz@gmail.com>
2019-03-29 17:37:30 -0300

    Add Random class

--- src/lib/base.js
+++ src/lib/base.js
@@ -132,8 +132,54 @@ export class HashMap {
     }
 }
 
-
 export function getChance(percentage) {
     let value = _.random(1.0, true)
     return value <= percentage
 }
+
+
+export class Random {
+    constructor(seed) {
+        this.seed = this._hash(seed)
+        this._s = this.seed
+    }
+
+    _hash(seed) {
+        let h = 61 ^ seed ^ seed >>> 16
+        h += h << 3
+        h = Math.imul(h, 668265261)
+        h ^= h >>> 15
+        return h >>> 0;
+    }
+
+    choice(items) {
+        let index = this.int(0, items.length-1)
+        return items[index]
+    }
+
+    int(lower=1, upper) {
+        let num = this.float()
+
+        lower = _.toFinite(lower)
+        if (upper === undefined) {
+            upper = lower
+            lower = 0
+        } else {
+            upper = _.toFinite(upper)
+        }
+
+        if (lower > upper) {
+            [lower, upper] = [upper, lower]
+        }
+        return lower + Math.floor(num * (upper - lower + 1))
+    }
+
+    float() {
+        this._s = this._s + 1831565813 | 0
+        let t = Math.imul(this._s ^ this._s >>> 15, 1 | this._s)
+        t = t + Math.imul(t ^ t >>> 7, 61 | t) ^ t
+        return ((t ^ t >>> 14) >>> 0) / 2 ** 32;
+    }
+}
+
+window.Random = Random
