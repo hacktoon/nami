b3ad1d3b111783358510e84bd00a6c50cb6431a3
Karlisson Bezerra <macedobz@gmail.com>
2019-03-30 14:18:14 -0300

    Add tilemap to tile mod

--- src/world/tile.js
+++ src/world/tile.js
@@ -9,3 +9,29 @@ export default class Tile {
         this.lastState = undefined
     }
 }
+
+
+class TileMap {
+    static get(id = null) {
+        const _map = [
+            { id: 0, color: "", name: "Ocean" },
+            { id: 1, color: "", name: "Coral" },
+            { id: 2, color: "", name: "River" },
+            { id: 3, color: "", name: "Lake" },
+            { id: 4, color: "", name: "Beach" },
+            { id: 5, color: "", name: "Tundra" },
+            { id: 6, color: "", name: "Taiga" },
+            { id: 7, color: "", name: "Grassland" },
+            { id: 8, color: "", name: "Forest" },
+            { id: 9, color: "", name: "Mangrove" },
+            { id: 10, color: "", name: "Jungle" },
+            { id: 11, color: "", name: "Shrubland" },
+            { id: 12, color: "", name: "Desert" }
+        ]
+        if (_.isNumber(id)) {
+            let index = _.clamp(id, 0, _map.length - 1)
+            return _map[index]
+        }
+        return _map
+    }
+}
