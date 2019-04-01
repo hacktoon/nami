cc4c079cf6b2b3e7be6b41aaf022a88ef6f07c3d
Karlisson Bezerra <macedobz@gmail.com>
2019-04-01 16:24:24 -0300

    Organize tile type

--- src/world/tile.js
+++ src/world/tile.js
@@ -2,20 +2,24 @@
 const TYPE_TABLE = (function() {
     return [
         { color: "", name: "Ocean" },
-        { color: "", name: "Sea" },
-        { color: "", name: "Beach" },
+        { color: "", name: "Litoral" },
         { color: "", name: "Coral" },
+        { color: "", name: "Beach" },
         { color: "", name: "River source" },
         { color: "", name: "River" },
         { color: "", name: "Lake" },
-        { color: "", name: "Swamp" },
         { color: "", name: "Mangrove" },
+        { color: "", name: "Swamp" },
         { color: "", name: "Grassland" },
         { color: "", name: "Shrubland" },
-        { color: "", name: "Desert" },
+        { color: "", name: "Savanna" },
         { color: "", name: "Forest" },
+        { color: "", name: "Taiga" },
         { color: "", name: "Jungle" },
+        { color: "", name: "Desert" },
         { color: "", name: "Tundra" },
+        { color: "", name: "Mountain" },
+        { color: "", name: "Peak" },
         { color: "#FFFFFF", name: "Iceberg" },
         { color: "#FFFFFF", name: "Ice cap" },
     ].map((obj, i) => { obj.id = i; return obj })
@@ -26,17 +30,15 @@ window.TYPE_TABLE = TYPE_TABLE
 export default class Tile {
     constructor (point) {
         this.point = point
-        this.name = ""
         this.type = undefined
-        this.lake = undefined
-        this.sea = undefined
-        this.river = undefined
-        this.ocean = undefined
+        this.lake = ""
+        this.sea = ""
+        this.river = ""
+        this.ocean = ""
+        this.volcano = ""
         this.isLand = true
         this.isWater = false
-        this.isRiverSource = false
-        this.isVolcano = false
-        this.lastState = undefined
+        this.state = undefined
     }
 
     setLake(lake) {
