821b6bde4e9f364f9954e34268ccfc24532087e0
Karlisson Bezerra <macedobz@gmail.com>
2019-08-26 18:33:39 -0300

    Reorganize maps hierarchy

--- src/world/geo/hydro/river.js
+++ src/world/geo/hydro/river.js
@@ -1,7 +1,7 @@
 
-import { Point } from '../../lib/point'
-import { MidpointDisplacement } from '../../lib/heightmap'
-import { FloodFill } from '../../lib/flood-fill'
+import { Point } from '../../../lib/point'
+import { MidpointDisplacement } from '../../../lib/heightmap'
+import { FloodFill } from '../../../lib/flood-fill'
 
 
 const SOURCE_CHANCE = .2                // chance of spawning a river source
