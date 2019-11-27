75b8cbe2ec6fa033bc786bdbc03c5be11bef229b
Karlisson Bezerra <macedobz@gmail.com>
2019-11-26 23:27:28 -0300

    Enable dragging on mouse move

--- src/ui/world/WorldView.js
+++ src/ui/world/WorldView.js
@@ -1,24 +1,18 @@
 import React, { useState, useLayoutEffect, useRef } from 'react'
+import { Point } from '/lib/point'
 
-
-const DEFAULT_TILE_SIZE = 3
+const DEFAULT_TILE_SIZE = 16
 
 
 export default function WorldView(props) {
     const [tilesize, setTilesize] = useState(DEFAULT_TILE_SIZE)
-    const [offset, setOffset] = useState([0, 0])
+    const [offset, setOffset] = useState(new Point(0, 0))
 
     const onTilesizeChange = event => setTilesize(event.target.value)
 
-    const onOffsetChange = (startPoint, endPoint) => {
-        const x = startPoint[0] - endPoint[0]
-        const y = startPoint[1] - endPoint[1]
-        setOffset([x + offset[0], y + offset[1]])
-    }
-
-    const drawFunction = (ctx, width, height) => {
-        console.log(offset)
-        drawWorld(props.world, ctx, tilesize)
+    const drawFunction = (context, width, height) => {
+        const options = { context, width, height, tilesize, offset }
+        drawWorld(props.world, options)
     }
 
     return <section id="world-view">
@@ -30,7 +24,8 @@ export default function WorldView(props) {
         <ViewPanel
             world={props.world}
             onReady={drawFunction}
-            onDrag={onOffsetChange}
+            onDrag={setOffset}
+            tilesize={tilesize}
         />
     </section>
 }
@@ -85,7 +80,7 @@ function ViewPanel(props) {
         <section className="screen" ref={viewportRef}>
             <canvas ref={canvasRef}></canvas>
         </section>
-        <TrackerPanel world={props.world} onDrag={props.onDrag}/>
+        <TrackerPanel world={props.world} onDrag={props.onDrag} tilesize={props.tilesize} />
     </section>
 }
 
@@ -93,31 +88,57 @@ function ViewPanel(props) {
 // TRACKING PANEL ================================================
 
 function TrackerPanel(props) {
-    const [point, setPoint] = useState([0, 0])
-    const [dragPoint, setDragPoint] = useState([0, 0])
+    const [coordinates, setCoordinates] = useState(new Point(0, 0))
+    const [offset, setOffset] = useState(new Point(0, 0))
+    const [startDragPoint, setStartDragPoint] = useState(new Point(0, 0))
     const [dragging, setDragging] = useState(false)
+    let timeoutId
 
-    const onMouseMove = event => setPoint(getEventPoint(event))
-    const onMouseLeave = () => setDragging(false)
+    const onMouseMove = event => {
+        let currentPoint = getEventPoint(event)
+        let x = currentPoint.x + offset.x
+        let y = currentPoint.y + offset.y
+        let newPoint = new Point(x, y)
+
+        if (dragging) {
+            clearTimeout(timeoutId)
+            timeoutId = setTimeout(() => {
+                const x = startDragPoint.x - currentPoint.x + offset.x
+                const y = startDragPoint.y - currentPoint.y + offset.y
+                newPoint = new Point(x, y)
+                props.onDrag(newPoint)
+            }, 60)
+        }
+        updateCoordinates(newPoint)
+    }
+
+    const updateCoordinates = point => {
+        const tilesize = props.tilesize
+        const gridX = Math.floor(point.x / tilesize)
+        const gridY = Math.floor(point.y / tilesize)
+        setCoordinates(new Point(gridX, gridY))
+    }
+
+    const onMouseUp = event => {
+        const currentPoint = getEventPoint(event)
+        const x = startDragPoint.x - currentPoint.x
+        const y = startDragPoint.y - currentPoint.y
+        setOffset(new Point(x + offset.x, y + offset.y))
+        setDragging(false)
+        clearTimeout(timeoutId)
+    }
 
     const onMouseDown = event => {
         event.preventDefault()
         setDragging(true)
-        setDragPoint(getEventPoint(event))
+        setStartDragPoint(getEventPoint(event))
     }
 
-    const onMouseUp = event => {
-        if (! dragging)
-            return
-        const startPoint = dragPoint
-        const endPoint = getEventPoint(event)
-        props.onDrag(startPoint, endPoint)
-        setDragging(false)
-    }
+    const onMouseLeave = () => setDragging(false)
 
     const getEventPoint = event => {
         const {offsetX: x, offsetY: y} = event.nativeEvent
-        return [x, y]
+        return new Point(x, y)
     }
 
     return <section className="tracker"
@@ -125,22 +146,32 @@ function TrackerPanel(props) {
         onMouseUp={onMouseUp}
         onMouseLeave={onMouseLeave}
         onMouseMove={onMouseMove}>
-            {point[0]}, {point[1]} {dragging ? ', dragging' : ''}
+            {coordinates.x}, {coordinates.y}
     </section>
 }
 
 
-const drawWorld = (world, ctx, tilesize) => {
-    world.iter((tile, point) => {
-        const color = world.reliefMap.codeMap.getColor(point)
-        drawWorldTile(ctx, point, tilesize, color)
-    })
+const drawWorld = (world, options) => {
+    const { context, width, height, tilesize, offset } = options
+    const {x: offsetX, y: offsetY} = offset
+    const subGridWidth = Math.ceil(width / tilesize)
+    const subGridHeight = Math.ceil(height / tilesize)
+    const gridOffsetX = Math.floor(offsetX / tilesize)
+    const gridOffsetY = Math.floor(offsetY / tilesize)
+
+    for(let i = 0; i < subGridWidth; i++) {
+        for(let j = 0; j < subGridHeight; j++) {
+            const gridPoint = new Point(i + gridOffsetX, j + gridOffsetY)
+            const x = i * tilesize
+            const y = j * tilesize
+
+            const color = world.reliefMap.codeMap.getColor(gridPoint)
+            context.fillStyle = color
+            context.fillRect(x, y, tilesize, tilesize)
+        }
+    }
 }
 
-const drawWorldTile = (ctx, point, tilesize, color) => {
-    let x = point.x * tilesize,
-        y = point.y * tilesize
+const drawWorldTile = (context, point, tilesize, color) => {
 
-    ctx.fillStyle = color
-    ctx.fillRect(x, y, tilesize, tilesize)
 }
