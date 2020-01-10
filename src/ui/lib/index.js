414f01c58f4d651326ee5383f2d29c95882f66ec
Karlisson Bezerra <macedobz@gmail.com>
2020-01-10 01:08:20 -0300

    Add classname func

--- src/ui/lib/index.js
+++ src/ui/lib/index.js
@@ -3,33 +3,48 @@ import React from 'react'
 
 // HELPER FUNCTIONS ==============================================
 
+function cls(...classNames) {
+    const names = classNames.filter(name => {
+        return Boolean(name)
+    });
+    return names.join(' ')
+}
+
 
 // GENERIC WIDGETS ===============================================
 
 export function Form(props) {
     const {className, layout, ...formProps} = props
-    return <section className={`${className||''} Form ${layout}`} {...formProps}>
+    return <section className={cls(className, 'Form', layout)} {...formProps}>
+        {props.children}
+    </section>
+}
+
+export function FormRow(props) {
+    const {className, layout, ...formProps} = props
+    const _className = cls(className, 'FormRow', 'Form', layout)
+    return <section className={_className} {...formProps}>
         {props.children}
     </section>
 }
 
 export function Grid(props) {
     const {className, ...gridProps} = props
-    return <section className={`${className} Grid`} {...gridProps}>
+    return <section className={cls(className, 'Grid')} {...gridProps}>
         {props.children}
     </section>
 }
 
 export function Row(props) {
     const {className, ...rowProps} = props
-    return <section className={`${className} Row`} {...rowProps}>
+    return <section className={cls(className, 'Row')} {...rowProps}>
         {props.children}
     </section>
 }
 
 export function Column(props) {
     const {className, ...colProps} = props
-    return <section className={`${className} Column`} {...colProps}>
+    return <section className={cls(className, 'Column')} {...colProps}>
         {props.children}
     </section>
 }
