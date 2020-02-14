be96179a4bd97ffb7d84968a71e3b76d86975013
Karlisson Bezerra <macedobz@gmail.com>
2020-02-13 22:49:21 -0300

    Add Tectonics model

---
+++ src/model/world/geo/tectonics.js
@@ -0,0 +1,16 @@
+import _ from 'lodash'
+
+import { Grid } from '/lib/grid'
+import { TileableHeightMap } from '/lib/heightmap'
+
+
+export class TectonicsMap {
+    constructor() {
+    }
+}
+
+
+class Tectonics {
+    constructor() {
+    }
+}
