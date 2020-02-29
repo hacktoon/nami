e388b1100c431f92d90982b9df5861a0ac320e96
Karlisson Bezerra <macedobz@gmail.com>
2020-02-29 20:16:17 -0300

    Extract paint config

--- src/ui/world/view/index.js
+++ src/ui/world/view/index.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { View } from './view'
 import { Menu } from './menu'
 import { Component } from '/ui/lib'
-import { Point } from '/lib/point'
+import { PaintConfig } from '/ui/lib/canvas'
 
 
 const DEFAULT_TILE_SIZE = 10
@@ -25,31 +25,6 @@ export default function WorldView(props) {
         />
         <View painter={painter} />
     </Component>
-}
-
-
-class PaintConfig {
-    constructor(config={}) {
-        this.canvas = config.canvas || <canvas />
-        this.offset = config.offset || new Point(0, 0)
-        this.width = Number(config.width)
-        this.height = Number(config.height)
-        this.tilesize = Number(config.tilesize)
-    }
-
-    get gridWidth() {
-        return Math.ceil(this.width / this.tilesize)
-    }
-
-    get gridHeight() {
-        return Math.ceil(this.height / this.tilesize)
-    }
-
-    getGridPoint(i, j) {
-        const x = Math.floor(this.offset.x / this.tilesize)
-        const y = Math.floor(this.offset.y / this.tilesize)
-        return new Point(x + i, y + j)
-    }
 }
 
 
