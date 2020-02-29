ce15ec02d5b17c299511ed5723ed2c89a8e3955a
Karlisson Bezerra <macedobz@gmail.com>
2020-02-29 00:55:13 -0300

    Update regions app

--- src/ui/regions/index.js
+++ src/ui/regions/index.js
@@ -1,20 +1,14 @@
 import React, { useState } from 'react'
 
-import Regions from '/model/regions'
-import RegionsMenu from './menu'
-import RegionsView from './view'
+
 import { Component } from '/ui/lib'
 
 import "./index.css"
 
 
 export default function RegionsApp(props) {
-    let [regions, setRegions] = useState(new Regions())
 
-    const onConfigChange = config => setRegions(new Regions(config))
+    return <Component className='RegionsApp'>   etc
 
-    return <Component className='RegionsApp'>
-        <RegionsMenu onChange={onConfigChange} />
-        <RegionsView regions={regions} />
     </Component>
 }
