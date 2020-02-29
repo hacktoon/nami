9e5f064ccf402549770489954652434a4c478e08
Karlisson Bezerra <macedobz@gmail.com>
2020-02-29 17:14:11 -0300

    Add field

--- src/ui/regions/menu.js
+++ src/ui/regions/menu.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { NumberField } from '../lib/field'
 import { Form, Button } from '../lib'
 
 
 export default function RegionsMenu(props) {
+    let [points, setPoints] = useState(8)
+
+    const onPointsChange = event => {
+        const points = Number(event.target.value)
+        setPoints(points)
+        // props.onChange(config)
+    }
+
     let onSubmit = () => {}
+
     return <Form className="RegionsMenu" onSubmit={onSubmit}>
+        <NumberField
+            label="Points"
+            value={points}
+            onChange={onPointsChange}
+            step={1}
+            min={1}
+        />
         <Button  text="Build" />
     </Form>
 }
