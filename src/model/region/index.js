4fe765e254db3ea92db9ae880b435279f88f53d2
Karlisson Bezerra <macedobz@gmail.com>
2020-03-21 19:25:00 -0300

    Resume Region model

--- src/model/region/index.js
+++ src/model/region/index.js
@@ -1,14 +1,6 @@
 
 export default class Region {
-    constructor() {
-        this.points = []
-    }
-
-    iter(callback) {
-        this.points.forEach(callback)
-    }
-
-    get(point) {
-        return this.points.get(point)
+    constructor(points) {
+        this.points = points
     }
 }
