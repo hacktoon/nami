5e77fda22be7d3007d63ac4697a17085f5e4fed9
Karlisson Bezerra <macedobz@gmail.com>
2020-04-06 21:50:13 -0300

    Add a few tests for number functions

--- src/lib/number/test.js
+++ src/lib/number/test.js
@@ -1,6 +1,18 @@
-import { sum } from './'
+import { interpolateNumbers, clamp, sum } from '.'
 
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum([1, 2])).toBe(3);
-});
+    expect(sum([1, 2])).toBe(3)
+})
+
+
+test('number interpolation', () => {
+    const list = interpolateNumbers(1, 4, 4)
+    expect(list).toStrictEqual([1, 2, 3, 4])
+})
+
+
+test('clamping', () => {
+    expect(clamp(10, 0, 4)).toBe(4)
+    expect(clamp(-3, 1, 4)).toBe(1)
+})
