9c4a5129a282d3717fe0163cb1d7b5e60d633a0a
Karlisson Bezerra <macedobz@gmail.com>
2020-05-14 19:37:07 -0300

    Simplify grid display

--- src/lib/ui/display.js
+++ src/lib/ui/display.js
@@ -6,62 +6,48 @@ import { Canvas } from '/lib/ui/canvas'
 
 
 export function GridDisplay(props) {
+    const {colorAt, tilesize, wrapMode} = props
     const [offset, setOffset] = useState(new Point(0, 0))
 
     const onDrag = pixelOffset => {
-        let x = Math.floor(pixelOffset.x / props.tilesize)
-        let y = Math.floor(pixelOffset.y / props.tilesize)
+        let x = Math.floor(pixelOffset.x / tilesize)
+        let y = Math.floor(pixelOffset.y / tilesize)
         setOffset(new Point(x, y))
     }
 
     // TODO: canvas must return a wrapped context object
     const onCanvasSetup = canvas => {
-        const gridRender = new GridRender({
-            canvas, offset, tilesize: props.tilesize
-        })
+        let totalColumns = Math.ceil(canvas.width / tilesize)
+        let totalRows = Math.ceil(canvas.height / tilesize)
 
-        for(let i = 0; i < gridRender.gridWidthSpan; i++) {
-            for(let j = 0; j < gridRender.gridHeightSpan; j++) {
-                renderCell(i, j, gridRender)
+        const inRange = point => {
+            if (wrapMode) return true
+            // TODO: this won't work, must have grid dimensions
+            const col = point.x >= 0 && point.x < totalColumns
+            const row = point.y >= 0 && point.y < totalRows
+            return col && row
+        }
+
+        for(let i = 0; i < totalColumns; i++) {
+            for(let j = 0; j < totalRows; j++) {
+                const point = new Point(offset.x + i, offset.y + j)
+                const color = inRange(point) ? colorAt(point) : 'black'
+                renderCell(canvas.context, i, j, color, tilesize)
             }
         }
     }
 
-    const renderCell = (i, j, gridRender) => {
-        const point = new Point(offset.x + i, offset.y + j)
-        const x = i * gridRender.tilesize
-        const y = j * gridRender.tilesize
-        let color = props.colorAt(point)
+    const renderCell = (context, i, j, color, tilesize) => {
+        const x = i * tilesize
+        const y = j * tilesize
 
-        gridRender.drawCell(x, y, color)
+        context.fillStyle = color
+        context.fillRect(x, y, tilesize, tilesize)
     }
 
     return <section className="GridDisplay">
+        {/* <GridTracker onMove={onMove} /> */}
         <MouseTracker onDrag={onDrag} />
         <Canvas onSetup={onCanvasSetup} />
     </section>
 }
-
-
-class GridRender {
-    constructor(config={}) {
-        this.context   = config.canvas.context
-        this.width    = Number(config.canvas.width)
-        this.height   = Number(config.canvas.height)
-        this.tilesize = Number(config.tilesize)
-    }
-
-    get gridWidthSpan() {
-        return Math.ceil(this.width / this.tilesize)
-    }
-
-    get gridHeightSpan() {
-        return Math.ceil(this.height / this.tilesize)
-    }
-
-    drawCell(x, y, color) {
-        this.context.fillStyle = color
-        this.context.fillRect(x, y, this.tilesize, this.tilesize)
-    }
-}
-
