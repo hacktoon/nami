b0f57f0a8c9e7ea55c0e825cfc333ed4e24849a2
Karlisson Bezerra <macedobz@gmail.com>
2020-06-21 22:53:57 -0300

    Fix color input hint dispĺay

--- src/lib/ui/form/field.js
+++ src/lib/ui/form/field.js
@@ -3,20 +3,7 @@ import React, { useMemo, useState } from 'react'
 import { Color } from '/lib/color'
 
 
-export function BooleanField({ id, label, value, onChange, ...props }) {
-    const [status, setStatus] = useState(Boolean(value))
-    const onClick = () => {
-        props.onChange(!status)
-        setStatus(!status)
-    }
-    return <Field key={id} type='boolean' label={label} status={status}>
-        <button onClick={onClick}>{status ? 'Yes' : 'No'}</button>
-        <input type='checkbox' checked={status} onChange={()=>{}} {...props} />
-    </Field>
-}
-
-
-export function NumberField({ id, value, label, onChange, ...props }) {
+export function NumberField({id, value, label, onChange, ...props}) {
     const handleChange = event => onChange(Number(event.currentTarget.value))
     return <Field key={id} type='number' label={label} {...props}>
         <input type='number' defaultValue={value} onChange={handleChange} {...props} />
@@ -24,25 +11,15 @@ export function NumberField({ id, value, label, onChange, ...props }) {
 }
 
 
-export function TextField({id, value, label, ...props }) {
-    const onChange = event => props.onChange(String(event.currentTarget.value))
+export function TextField({id, value, label, onChange, ...props}) {
+    const handleChange = event => onChange(String(event.currentTarget.value))
     return <Field key={id} type='text' label={label}>
-        <input type='text' defaultValue={value} onChange={onChange} {...props} />
+        <input type='text' defaultValue={value} {...props} onChange={handleChange} />
     </Field>
 }
 
 
-export function ColorField({id, value, label, ...props }) {
-    const onChange = event => props.onChange(Color.fromHex(event.currentTarget.value))
-    return <Field key={id} type='text' label={label}>
-        <div className='ColorFieldValue'>
-            <input type='text' defaultValue={value.toHex()} onChange={onChange} {...props} />
-        </div>
-    </Field>
-}
-
-
-export function SelectField({id, value, label, options, ...props }) {
+export function SelectField({id, value, label, options, ...props}) {
     function buildSelectOptions(options) {
         const entries = Object.entries(options)
         return entries.map((option, index) => {
@@ -57,18 +34,37 @@ export function SelectField({id, value, label, options, ...props }) {
     </Field>
 }
 
-// TODO: remove
-export function OutputField({ label, value }) {
-    return <section className='Field'>
-        <output className='FieldLabel'>{label}</output>
-        <output className='FieldValue'>{value}</output>
-    </section>
+
+export function BooleanField({id, label, value, onChange, ...props}) {
+    const [status, setStatus] = useState(Boolean(value))
+    const onClick = () => {
+        onChange(!status)
+        setStatus(!status)
+    }
+    return <Field key={id} type='boolean' label={label} status={status}>
+        <button onClick={onClick}>{status ? 'Yes' : 'No'}</button>
+        <input type='checkbox' checked={status} onChange={()=>{}} {...props} />
+    </Field>
+}
+
+
+export function ColorField({id, value, label, onChange, ...props}) {
+    const [color, setColor] = useState(value)
+    const handleChange = event => {
+        const newColor = Color.fromHex(event.currentTarget.value)
+        onChange(newColor)
+        setColor(newColor)
+    }
+    return <Field key={id} type='color' label={label}>
+        <span className="ColorView" style={{backgroundColor: color.toHex()}}></span>
+        <input type='text' defaultValue={color.toHex()} onChange={handleChange} {...props} />
+    </Field>
 }
 
 
 // BASE FIELD COMPONENT ===============================================
 
-function Field({ label, type, status = '', children, ...props }) {
+function Field({ label, type, status = '', children, ...props}) {
     return <label className={`Field ${type} ${status}`} {...props}>
         <span className='FieldLabel'>{label}</span>
         <span className='FieldValue'>{children}</span>
@@ -81,7 +77,7 @@ function Field({ label, type, status = '', children, ...props }) {
 export function buildFields(fields) {
     return fields.map(({ type, ...props }, id) => {
         const FieldComponent = TYPE_FIELD_MAP[type]
-        return FieldComponent({ id, onChange: () => { }, ...props })
+        return FieldComponent({id, onChange: ()=>{}, ...props})
     })
 }
 
