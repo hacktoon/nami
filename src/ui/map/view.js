388a1cb6ade46a7c8c5e6caa44fa50e047ef6e44
Karlisson Bezerra <macedobz@gmail.com>
2020-07-11 16:16:37 -0300

    Extract functions

--- src/ui/map/view.js
+++ src/ui/map/view.js
@@ -6,53 +6,29 @@ import { Canvas } from '/lib/ui/canvas'
 
 
 export function MapView({image}) {
+    const [focus, setFocus] = useState(new Point(0, 0))
     const [offset, setOffset] = useState(new Point(0, 0))
     const onDrag = offset => setOffset(new Point(offset.x, offset.y))
 
-    const getWindow = canvas => {
-        let width = Math.ceil(canvas.width / image.tileSize)
-        let height = Math.ceil(canvas.height / image.tileSize)
-        return [width, height]
-    }
-
     const renderForeground = (i, j, canvas) => {
-        let [offsetX, offsetY] = gridOffset()
-        const point = new Point(offsetX + i, offsetY + j)
-        if (isWrappable(point)) {
+        const _offset = calcGridOffset(offset, image)
+        const point = new Point(_offset.x + i, _offset.y + j)
+        if (isWrappable(image, point)) {
             const color = image.get(point)
             renderCell(canvas.context, i, j, color, image.tileSize)
         }
     }
 
     const renderBackground = (i, j, canvas) => {
-        let [offsetX, offsetY] = gridOffset()
-        const point = new Point(offsetX + i, offsetY + j)
-        if (!isWrappable(point) && (i + j) % 2) {
+        const _offset = calcGridOffset(offset, image)
+        const point = new Point(_offset.x + i, _offset.y + j)
+        if (!isWrappable(image, point) && (i + j) % 2) {
             renderCell(canvas.context, i, j, '#FFF', image.tileSize)
         }
     }
 
-    const gridOffset = () => [
-        Math.floor(offset.x / image.tileSize),
-        Math.floor(offset.y / image.tileSize)
-    ]
-
-    const isWrappable = point => {
-        if (image.wrapMode) return true
-        const col = point.x >= 0 && point.x < image.width
-        const row = point.y >= 0 && point.y < image.height
-        return col && row
-    }
-
-    const renderCell = (context, i, j, color, tileSize) => {
-        const x = i * tileSize
-        const y = j * tileSize
-        context.fillStyle = color
-        context.fillRect(x, y, tileSize, tileSize)
-    }
-
     const onCanvasSetup = canvas => {
-        let [width, height] = getWindow(canvas)
+        let [width, height] = getTileWindow(canvas, image)
         for(let i = 0; i < width; i++) {
             for(let j = 0; j < height; j++) {
                 renderForeground(i, j, canvas)
@@ -61,7 +37,7 @@ export function MapView({image}) {
     }
 
     const onBackgroundCanvasSetup = canvas => {
-        let [width, height] = getWindow(canvas)
+        let [width, height] = getTileWindow(canvas, image)
         for(let i = 0; i < width; i++) {
             for(let j = 0; j < height; j++) {
                 renderBackground(i, j, canvas)
@@ -76,6 +52,43 @@ export function MapView({image}) {
             <Canvas onInit={onBackgroundCanvasSetup} />
         </div>
     </section>
+}
+
+function renderCell(context, i, j, color, tileSize) {
+    const x = i * tileSize
+    const y = j * tileSize
+    context.fillStyle = color
+    context.fillRect(x, y, tileSize, tileSize)
+}
+
+function isWrappable(image, point) {
+    if (image.wrapMode) return true
+    const col = point.x >= 0 && point.x < image.width
+    const row = point.y >= 0 && point.y < image.height
+    return col && row
+}
+
+function calcGridOffset(offset, image) {
+    return new Point(
+        Math.floor(offset.x / image.tileSize),
+        Math.floor(offset.y / image.tileSize)
+    )
+}
+
+function getTileWindow(canvas, image) {
+    const width = Math.ceil(canvas.width / image.tileSize)
+    const height = Math.ceil(canvas.height / image.tileSize)
+    return [width, height]
+}
+
+
+
+export function Layer({image}) {
+
+}
+
+export function BGLayer({tileSize}) {
+
 }
 
 
