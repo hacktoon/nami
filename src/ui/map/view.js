dff0e56d6cf22543a9b143c82c3812ff4deed5e4
Karlisson Bezerra <macedobz@gmail.com>
2020-07-11 16:56:52 -0300

    Break MapView into components

--- src/ui/map/view.js
+++ src/ui/map/view.js
@@ -7,10 +7,28 @@ import { Canvas } from '/lib/ui/canvas'
 
 export function MapView({image}) {
     const [focus, setFocus] = useState(new Point(0, 0))
+
+    return <section className="MapView">
+        <Layer image={image} />
+        <BGLayer image={image} />
+    </section>
+}
+
+
+export function Layer({image}) {
     const [offset, setOffset] = useState(new Point(0, 0))
     const onDrag = offset => setOffset(new Point(offset.x, offset.y))
 
-    const renderForeground = (i, j, canvas) => {
+    const onCanvasSetup = canvas => {
+        let [width, height] = getTileWindow(canvas, image)
+        for(let i = 0; i < width; i++) {
+            for(let j = 0; j < height; j++) {
+                renderForeground(image, offset, i, j, canvas)
+            }
+        }
+    }
+
+    const renderForeground = (image, offset, i, j, canvas) => {
         const _offset = calcGridOffset(offset, image)
         const point = new Point(_offset.x + i, _offset.y + j)
         if (isWrappable(image, point)) {
@@ -19,53 +37,10 @@ export function MapView({image}) {
         }
     }
 
-    const renderBackground = (i, j, canvas) => {
-        const _offset = calcGridOffset(offset, image)
-        const point = new Point(_offset.x + i, _offset.y + j)
-        if (!isWrappable(image, point) && (i + j) % 2) {
-            renderCell(canvas.context, i, j, '#FFF', image.tileSize)
-        }
-    }
-
-    const onCanvasSetup = canvas => {
-        let [width, height] = getTileWindow(canvas, image)
-        for(let i = 0; i < width; i++) {
-            for(let j = 0; j < height; j++) {
-                renderForeground(i, j, canvas)
-            }
-        }
-    }
-
-    const onBackgroundCanvasSetup = canvas => {
-        let [width, height] = getTileWindow(canvas, image)
-        for(let i = 0; i < width; i++) {
-            for(let j = 0; j < height; j++) {
-                renderBackground(i, j, canvas)
-            }
-        }
-    }
-
-    return <section className="MapView">
+    return <>
         <MouseView onDrag={onDrag} />
         <Canvas onInit={onCanvasSetup} />
-        <div className="BackgroundCanvas">
-            <Canvas onInit={onBackgroundCanvasSetup} />
-        </div>
-    </section>
-}
-
-function renderCell(context, i, j, color, tileSize) {
-    const x = i * tileSize
-    const y = j * tileSize
-    context.fillStyle = color
-    context.fillRect(x, y, tileSize, tileSize)
-}
-
-function isWrappable(image, point) {
-    if (image.wrapMode) return true
-    const col = point.x >= 0 && point.x < image.width
-    const row = point.y >= 0 && point.y < image.height
-    return col && row
+    </>
 }
 
 function calcGridOffset(offset, image) {
@@ -75,21 +50,54 @@ function calcGridOffset(offset, image) {
     )
 }
 
+
+export function BGLayer({image}) {
+    const onBackgroundCanvasSetup = canvas => {
+        let [width, height] = getTileWindow(canvas, image)
+        for(let i = 0; i < width; i++) {
+            for(let j = 0; j < height; j++) {
+                renderBackground(image, i, j, canvas)
+            }
+        }
+    }
+
+    const renderBackground = (image, i, j, canvas) => {
+        if ((i + j) % 2) {
+            renderCell(canvas.context, i, j, '#FFF', image.tileSize)
+        }
+    }
+
+    return <div className="BackgroundCanvas">
+        <Canvas onInit={onBackgroundCanvasSetup} />
+    </div>
+}
+
+
+function renderCell(context, i, j, color, tileSize) {
+    const x = i * tileSize
+    const y = j * tileSize
+    context.fillStyle = color
+    context.fillRect(x, y, tileSize, tileSize)
+}
+
+
+function isWrappable(image, point) {
+    if (image.wrapMode) return true
+    const col = point.x >= 0 && point.x < image.width
+    const row = point.y >= 0 && point.y < image.height
+    return col && row
+}
+
+
+
+
+
 function getTileWindow(canvas, image) {
     const width = Math.ceil(canvas.width / image.tileSize)
     const height = Math.ceil(canvas.height / image.tileSize)
     return [width, height]
 }
 
-
-
-export function Layer({image}) {
-
-}
-
-export function BGLayer({tileSize}) {
-
-}
 
 
 function createCanvas(originalCanvas) {
