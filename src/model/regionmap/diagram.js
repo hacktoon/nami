591ed9d0be8a5d2a403c607114964e9dd04cc82a
Karlisson Bezerra <macedobz@gmail.com>
2021-01-22 20:47:51 -0300

    Adjust diagram labels

--- src/model/regionmap/diagram.js
+++ src/model/regionmap/diagram.js
@@ -5,10 +5,10 @@ import { BaseMapDiagram } from '/model/lib/map'
 
 export class MapDiagram extends BaseMapDiagram {
     static schema = new Schema(
-        Type.boolean('showBorder', 'Show border', true),
-        Type.boolean('showOrigin', 'Show origin', true),
-        Type.boolean('invert', 'Invert colors', false),
-        Type.number('layer', 'Layer', 3, {step: 1, min: 0}),
+        Type.boolean('showBorders', 'Show borders', true),
+        Type.boolean('showOrigins', 'Show origins', true),
+        Type.boolean('invertColors', 'Invert colors', false),
+        Type.number('showLayer', 'Show layer', 3, {step: 1, min: 0}),
         Type.number('showRegion', 'Show region', -1, {step: 1, min: -1}),
         Type.color('foreground', 'Foreground', Color.fromHex('#251')),
         Type.color('background', 'Background', Color.fromHex('#059')),
@@ -22,34 +22,34 @@ export class MapDiagram extends BaseMapDiagram {
     constructor(map, params) {
         super(map)
         // TODO: set `this.data` and add attributes dynamically
-        this.showBorder = params.get('showBorder')
-        this.showOrigin = params.get('showOrigin')
-        this.layer = params.get('layer')
+        this.showBorders = params.get('showBorders')
+        this.showOrigins = params.get('showOrigins')
+        this.showLayer = params.get('showLayer')
         this.foreground = params.get('foreground')
         this.background = params.get('background')
         this.borderColor = params.get('borderColor')
         this.showRegion = params.get('showRegion')
-        this.invert = params.get('invert')
+        this.invertColors = params.get('invertColors')
     }
 
     get(point) {
-        if (this.showBorder && this.map.isBorder(point)) {
+        if (this.showBorders && this.map.isBorder(point)) {
             if (this.map.regionAt(point).id === this.showRegion)
                 return this.borderColor.invert().toHex()
             return this.borderColor.toHex()
         }
-        if (this.showOrigin && this.map.isOrigin(point)) {
+        if (this.showOrigins && this.map.isOrigin(point)) {
             return this.foreground.invert().toHex()
         }
         // draw seed
-        if (this.map.isLayer(point, this.layer)) {
+        if (this.map.isLayer(point, this.showLayer)) {
             return this.foreground.brighten(40).toHex()
         }
 
         const pointLayer = this.map.at(point).layer
-        const background = this.invert ? this.background : this.foreground
-        const foreground = this.invert ? this.foreground : this.background
-        if (this.map.isOverLayer(point, this.layer)) {
+        const background = this.invertColors ? this.background : this.foreground
+        const foreground = this.invertColors ? this.foreground : this.background
+        if (this.map.isOverLayer(point, this.showLayer)) {
             return foreground.darken(pointLayer*5).toHex()
         } else {
             return background.darken(pointLayer*5).toHex()
