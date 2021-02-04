6fcfea7be8a021be30794d6f64192ee8ae8864b7
Karlisson Bezerra <macedobz@gmail.com>
2021-02-03 23:27:42 -0300

    Test 8 neighbors from level 2 base flood fill

--- src/lib/flood-fill/base.js
+++ src/lib/flood-fill/base.js
@@ -5,27 +5,19 @@ export class BaseFloodFill {
         this.setValue = params.setValue
         this.isEmpty = params.isEmpty
 
-        this.growSeeds()
+        this.setValue(this.origin)
     }
 
     grow() {
-        this.spreadSeeds()
-        this.growSeeds()
-        return this.seeds
-    }
-
-    spreadSeeds() {
         let seeds = []
         this.seeds.forEach(point => {
-            point.adjacents().forEach(seedCandidate => {
-                if (! this.isEmpty(seedCandidate)) return
-                seeds.push(seedCandidate)
+            point.adjacents().forEach(candidate => {
+                if (! this.isEmpty(candidate)) return
+                this.setValue(candidate)
+                seeds.push(candidate)
             })
         })
         this.seeds = seeds
-    }
-
-    growSeeds() {
-        this.seeds.forEach(point => this.setValue(point))
+        return seeds
     }
 }
