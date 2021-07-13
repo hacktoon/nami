fa27b6f726d4c7866c4e4ef1d0a5deaa99492bea
Karlisson Bezerra <macedobz@gmail.com>
2021-07-13 20:44:59 -0300

    Extract plates map

--- src/model/tilemap/world/geology/model.js
+++ src/model/tilemap/world/geology/model.js
@@ -1,41 +1,15 @@
-import { Random } from '/lib/base/random'
 import { MultiFill, FloodFillConfig } from '/lib/floodfill'
 import { OrganicFloodFill } from '/lib/floodfill/organic'
-import { Direction } from '/lib/base/direction'
 import { SimplexNoise } from '/lib/fractal/noise'
-
 import { RegionGroupTileMap } from '/model/tilemap/regiongroup'
 import { BoundaryMap } from './boundary'
-
-
-const TYPE_CONTINENTAL = 'L'
-const TYPE_OCEANIC = 'W'
-
-
-export class Plate {
-    constructor(id, origin, type, area, weight) {
-        this.id = id
-        this.type = type
-        this.area = area
-        this.origin = origin
-        this.direction = Direction.random()
-        this.weight = weight
-    }
-
-    isOceanic() {
-        return this.type === TYPE_OCEANIC
-    }
-
-    isContinental() {
-        return this.type === TYPE_CONTINENTAL
-    }
-}
+import { PlateMap } from './plate'
 
 
 export class TectonicsModel {
     constructor(seed, params) {
         this.regionGroupTileMap = this._buildRegionGroupMap(seed, params)
-        this.plates = this._buildPlates()
+        this.plates = new PlateMap(this.regionGroupTileMap)
         this.stressLevels = new Map()
         this.regionBoundary = new Map()
 
@@ -47,7 +21,6 @@ export class TectonicsModel {
                 regionGroupTileMap: this.regionGroupTileMap,
                 regionBoundary: this.regionBoundary,
                 stressLevels: this.stressLevels,
-                plates: this.plates,
                 id: group.id,
                 boundary,
             })
@@ -72,37 +45,6 @@ export class TectonicsModel {
         })
     }
 
-    _buildPlates() {
-        const plates = new Map()
-        const cmpDescendingCount = (g0, g1) => g1.count - g0.count
-        const groups = this.regionGroupTileMap.getGroups().sort(cmpDescendingCount)
-        const typeMap = this._buildPlateTypes(groups)
-        groups.forEach(group => {
-            const neighborsGroups = this.regionGroupTileMap.getNeighborGroups(group)
-            const isLandlocked = neighborsGroups.concat(group).every(neighbor => {
-                return typeMap.get(neighbor.id) === TYPE_CONTINENTAL
-            })
-            const type = isLandlocked ? TYPE_OCEANIC : typeMap.get(group.id)
-            const weight = group.id + (type === TYPE_OCEANIC ? groups.length * 10 : 0)
-            const plate = new Plate(group.id, group.origin, type, group.area, weight)
-            plates.set(plate.id, plate)
-        })
-        return plates
-    }
-
-    _buildPlateTypes(groups) {
-        const halfWorldArea = Math.floor(this.regionGroupTileMap.area / 2)
-        const typeMap = new Map()
-        let totalOceanicArea = 0
-        groups.forEach(group => {
-            totalOceanicArea += group.area
-            const isOceanic = totalOceanicArea < halfWorldArea
-            const type = isOceanic ? TYPE_OCEANIC : TYPE_CONTINENTAL
-            typeMap.set(group.id, type)
-        })
-        return typeMap
-    }
-
     _buildPlateBoundary(boundaryMap, group, region) {
         let boundary
         const neighborRegions = this.regionGroupTileMap.getNeighborRegions(region)
@@ -116,7 +58,7 @@ export class TectonicsModel {
     }
 
     map(callback) {
-        return Array.from(this.plates.values()).map(plate => callback(plate))
+        return this.plates.map(plate => callback(plate))
     }
 
     forEach(callback) {
---
+++ src/model/tilemap/world/geology/plate.js
@@ -0,0 +1,73 @@
+import { Direction } from '/lib/base/direction'
+import { Random } from '/lib/base/random'
+
+
+const TYPE_CONTINENTAL = 'L'
+const TYPE_OCEANIC = 'W'
+
+
+class Plate {
+    constructor(id, origin, type, area, weight) {
+        this.id = id
+        this.type = type
+        this.area = area
+        this.origin = origin
+        this.direction = Direction.random()
+        this.weight = weight
+    }
+
+    isOceanic() {
+        return this.type === TYPE_OCEANIC
+    }
+
+    isContinental() {
+        return this.type === TYPE_CONTINENTAL
+    }
+}
+
+
+export class PlateMap {
+    constructor(regionGroupTileMap) {
+        this.regionGroupTileMap = regionGroupTileMap
+        this.plates = new Map()
+        const cmpDescendingCount = (g0, g1) => g1.count - g0.count
+        const groups = this.regionGroupTileMap.getGroups().sort(cmpDescendingCount)
+        const typeMap = this._buildTypes(groups)
+        groups.forEach(group => {
+            const neighborsGroups = this.regionGroupTileMap.getNeighborGroups(group)
+            const isLandlocked = neighborsGroups.concat(group).every(neighbor => {
+                return typeMap.get(neighbor.id) === TYPE_CONTINENTAL
+            })
+            const type = isLandlocked ? TYPE_OCEANIC : typeMap.get(group.id)
+            const weight = group.id + (type === TYPE_OCEANIC ? groups.length * 10 : 0)
+            const plate = new Plate(group.id, group.origin, type, group.area, weight)
+            this.plates.set(plate.id, plate)
+        })
+    }
+
+    _buildTypes(groups) {
+        const halfWorldArea = Math.floor(this.regionGroupTileMap.area / 2)
+        const typeMap = new Map()
+        let totalOceanicArea = 0
+        groups.forEach(group => {
+            totalOceanicArea += group.area
+            const isOceanic = totalOceanicArea < halfWorldArea
+            const type = isOceanic ? TYPE_OCEANIC : TYPE_CONTINENTAL
+            typeMap.set(group.id, type)
+        })
+        return typeMap
+    }
+
+    get(id) {
+        return this.plates.get(id)
+    }
+
+    map(callback) {
+        return Array.from(this.plates.values()).map(plate => callback(plate))
+    }
+
+    forEach(callback) {
+        this.plates.forEach(callback)
+    }
+}
+
