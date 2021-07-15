3572117499c66e70f7f360980abffc91d505781b
Karlisson Bezerra <macedobz@gmail.com>
2021-07-14 21:39:50 -0300

    Adjust orogeny

--- src/model/tilemap/world/geology/index.js
+++ src/model/tilemap/world/geology/index.js
@@ -13,7 +13,7 @@ const SCHEMA = new Schema(
     Type.number('height', 'Height', {default: 100, step: 1, min: 1, max: 500}),
     Type.number('scale', 'Scale', {default: 30, step: 1, min: 1, max: 100}),
     Type.number('growth', 'Growth', {default: 40, step: 1, min: 1, max: 100}),
-    Type.text('seed', 'Seed', {default: '1626308703603'})
+    Type.text('seed', 'Seed', {default: ''})
 )
 
 
