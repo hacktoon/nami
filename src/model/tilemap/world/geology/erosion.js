975a30addc4f6b1cea70f8edcfc5d6c28158b838
Karlisson Bezerra <macedobz@gmail.com>
2021-08-24 20:13:49 -0300

    Add rising landform chances

--- src/model/tilemap/world/geology/erosion.js
+++ src/model/tilemap/world/geology/erosion.js
@@ -30,69 +30,39 @@ export class ErosionModel {
 
 class ErosionMatrix {
     constructor(width, height, landformMatrix) {
-        let [matrix, queue] = this._erodeMatrix(width, height, landformMatrix)
-        // queue = this._erodeQueue(matrix, queue)
-        // queue = this._erodeQueue(matrix, queue)
-        // queue = this._erodeQueue(matrix, queue)
+        let matrix = this._erodeMatrix(width, height, landformMatrix)
+        matrix = this._erodeMatrix(width, height, matrix)
 
         this.matrix = matrix
     }
 
     _erodeMatrix(width, height, baseMatrix) {
-        const erosionQueue = []
-        const buildMatrix = centerPoint => {
-            // for each point in the matrix
-            let centerLandform = baseMatrix.get(centerPoint)
-            let highestSideLandform = centerLandform
-            let highestSidePoint = centerPoint
+        const buildMatrix = point => {
+            let landform = baseMatrix.get(point)
+            let highestSideLandform = landform
             let sameNeighborCount = 0
+            const sidePoints = point.adjacents()
             // visit each one
-            for(let sidePoint of centerPoint.adjacents()) {
+            for(let sidePoint of sidePoints) {
                 const sideLandform = baseMatrix.get(sidePoint)
                 // get the highest landform of sides
                 if (sideLandform.height > highestSideLandform.height) {
                     highestSideLandform = sideLandform
-                    highestSidePoint = sidePoint
                 }
                 // is it the same landform?
-                if (centerLandform.name == sideLandform.name) {
+                if (landform.name == sideLandform.name) {
                     sameNeighborCount++
                 }
             }
             // erode center based on side
-            if (Landform.canErode(centerLandform, highestSideLandform)) {
-                erosionQueue.push(highestSidePoint)
-                centerLandform = Landform.erode(centerLandform, highestSideLandform)
-            } else if (sameNeighborCount == 4) {
-                centerLandform = Landform.rise(centerLandform)
+            if (Landform.canErode(landform, highestSideLandform)) {
+                landform = Landform.erode(landform, highestSideLandform)
+            } else if (sameNeighborCount == sidePoints.length) {
+                landform = Landform.rise(landform)
             }
-            return centerLandform
+            return landform
         }
-        const matrix = new Matrix(width, height, buildMatrix)
-        console.log(erosionQueue.length);
-        // console.log(`${erosionQueue.length} points eroded`)
-        return [matrix, erosionQueue]
-    }
-
-    _erodeQueue(matrix, queue) {
-        const erosionQueue = []
-        queue.forEach(centerPoint => {
-            let centerLandform = matrix.get(centerPoint)
-            centerPoint.adjacents((sidePoint, direction) => {
-                const sideLandform = matrix.get(sidePoint)
-                // erode region by neighborhood
-                if (Landform.canErode(centerLandform, sideLandform)) {
-                    centerLandform = Landform.erode(centerLandform, sideLandform)
-                    erosionQueue.push(sidePoint)
-                }
-                return centerLandform.name == sideLandform.name
-            })
-        })
-        erosionQueue.forEach(point => {
-            // matrix.set(point, centerLandform)
-        })
-        console.log(erosionQueue.length, ' points eroded');
-        return erosionQueue
+        return new Matrix(width, height, buildMatrix)
     }
 
     get(point) {
--- src/model/tilemap/world/geology/landform.js
+++ src/model/tilemap/world/geology/landform.js
@@ -1,3 +1,5 @@
+import { Random } from '/lib/base/random'
+
 
 export const LANDFORMS = {
     SUMMIT: {
@@ -6,11 +8,11 @@ export const LANDFORMS = {
     },
     PEAK: {
         name: 'PEAK', water: false, height: 6, color: '#AAA',
-        erodesTo: 'MOUNTAIN', risesTo: 'SUMMIT'
+        erodesTo: 'MOUNTAIN', risesTo: 'SUMMIT', riseChance: .4
     },
     MOUNTAIN: {
         name: 'MOUNTAIN', water: false, height: 5, color: '#8b8372',
-        erodesTo: 'HILL'
+        erodesTo: 'HILL', risesTo: 'PEAK', riseChance: .05
     },
     PLATEAU: {
         name: 'PLATEAU', water: false, height: 4, color: '#996',
@@ -18,25 +20,26 @@ export const LANDFORMS = {
     },
     HILL: {
         name: 'HILL', water: false, height: 4, color: '#585',
-        erodesTo: 'PLAIN'
+        erodesTo: 'PLAIN', risesTo: 'MOUNTAIN', riseChance: .1
     },
     PLAIN: {
         name: 'PLAIN', water: false, height: 3, color: '#574',
-        erodesTo: 'SHALLOW_SEA'
+        erodesTo: 'SHALLOW_SEA', risesTo: 'HILL', riseChance: .02
     },
     ISLAND: {
-        name: 'ISLAND', water: false, height: 3, color: '#685',
-        erodesTo: 'SHALLOW_SEA'
+        name: 'ISLAND', water: false, height: 3, color: '#574',
+        erodesTo: 'SHALLOW_SEA', risesTo: 'HILL', riseChance: .4
     },
     DEPRESSION: {
         name: 'DEPRESSION', water: false, height: 2, color: '#352'
     },
     SHALLOW_SEA: {
         name: 'SHALLOW_SEA', water: true, height: 2, color: '#069',
-        erodesTo: 'DEEP_SEA'
+        erodesTo: 'DEEP_SEA', risesTo: 'ISLAND', riseChance: .01
     },
     DEEP_SEA: {
-        name: 'DEEP_SEA', water: true, height: 1, color: '#058'
+        name: 'DEEP_SEA', water: true, height: 1, color: '#058',
+        risesTo: 'SHALLOW_SEA', riseChance: .01
     },
     TRENCH: {
         name: 'TRENCH', water: true, height: 0, color: '#147'
@@ -54,8 +57,14 @@ export class Landform {
         return LANDFORMS[name] ?? centerLandform
     }
 
-    static rise(centerLandform) {
-        const name = centerLandform.risesTo
-        return LANDFORMS[name] ?? centerLandform
+    static rise(landform) {
+        const name = landform.risesTo
+        if (name) {
+            const risenLandform = LANDFORMS[name] ?? landform
+            if (Random.chance(landform.riseChance)) {
+                return risenLandform
+            }
+        }
+        return landform
     }
 }
