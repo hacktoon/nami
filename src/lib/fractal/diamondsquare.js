a92c6f66721d2af3485e08faf12323dee730bcc1
Karlisson Bezerra <macedobz@gmail.com>
2021-08-28 18:31:04 -0300

    Split fractal libs

--- src/lib/fractal/diamondsquare.js
+++ src/lib/fractal/diamondsquare.js
@@ -8,52 +8,6 @@ import { Random } from '/lib/base/random'
 const EMPTY = null
 
 
-export const MidpointDisplacement = (source, target, roughness, callback=()=>{}) => {
-    const deltaX = Math.abs(source.x - target.x)
-    const deltaY = Math.abs(source.y - target.y)
-    const fixedAxis = deltaX > deltaY ? 'x' : 'y'
-    const displacedAxis = deltaX > deltaY ? 'y' : 'x'
-    const size = Math.abs(target[fixedAxis] - source[fixedAxis])
-    let displacement = roughness * (size / 2)
-    let points = []
-
-    const buildPoint = (p1, p2) => {
-        if (Math.abs(p2[fixedAxis] - p1[fixedAxis]) <= 1)
-            return
-        const displacedValue = (p1[displacedAxis] + p2[displacedAxis]) / 2
-        const variance = Random.int(-displacement, displacement)
-        const point = new Point()
-
-        point[fixedAxis] = Math.floor((p1[fixedAxis] + p2[fixedAxis]) / 2)
-        point[displacedAxis] = Math.round(displacedValue + variance)
-        return point
-    }
-
-    const midpoints = (p1, p2, size) => {
-        let points = []
-        let point = buildPoint(p1, p2)
-        if (!point)
-            return points
-        displacement = roughness * size
-        points = points.concat(midpoints(p1, point, size / 2))
-        addPoint(point)
-        points = points.concat(midpoints(point, p2, size / 2))
-        return points
-    }
-
-    const addPoint = (point) => {
-        points.push(point)
-        callback(point)
-    }
-
-    addPoint(source)
-    points = points.concat(midpoints(source, target, size / 2))
-    addPoint(target)
-
-    return points
-}
-
-
 export class DiamondSquare {
     constructor(size, roughness, values) {
         this.scale  = roughness * (size - 1)
