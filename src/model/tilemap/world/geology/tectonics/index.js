6e4bb943e506d7bdf9ae17959434e028b6086e32
Karlisson Bezerra <macedobz@gmail.com>
2021-11-03 22:17:14 -0300

    Merge boundary into tectonics

--- src/model/tilemap/world/geology/tectonics/index.js
+++ src/model/tilemap/world/geology/tectonics/index.js
@@ -1,23 +1,59 @@
 import { GenericMultiFill, GenericFloodFill } from '/lib/floodfill'
+import { Direction } from '/lib/direction'
 
-import { BoundaryModel } from './boundary'
+import { Landform } from '../landform'
+import { BoundaryTable } from './table'
 
 
 export class TectonicsModel {
     #regionBoundaryMap = new Map()
     #landformMap = new Map()
+    #boundaries = []
+    #boundaryName = []
+    #boundaryLandscape = []
 
     constructor(realmTileMap, plateModel) {
         const borderRegions = realmTileMap.getBorderRegions()
-        this.boundaryModel = new BoundaryModel(borderRegions, plateModel, realmTileMap)
-        this.borderRegions = borderRegions
         this.realmTileMap = realmTileMap
         this.plateModel = plateModel
-        new BoundaryMultiFill(this).fill()
+        this.boundaryTable = new BoundaryTable(plateModel)
+        this._buildBoundaries(borderRegions)
+        new BoundaryMultiFill(this, borderRegions).fill()
+    }
+
+    _buildBoundaries(borderRegionIds) {
+        for(let id = 0; id < borderRegionIds.length; id ++) {
+            const regionId = borderRegionIds[id]
+            const [boundaryName, landscape] = this._getRegionBoundary(regionId)
+            this.#boundaries.push(id)
+            this.#boundaryName.push(boundaryName)
+            this.#boundaryLandscape.push(landscape)
+        }
+    }
+
+    _getRegionBoundary(regionId) {
+        const realmId = this.realmTileMap.getRealmByRegion(regionId)
+        const sideRegionIds = this.realmTileMap.getSideRegions(regionId)
+        for(let sideRegionId of sideRegionIds) {
+            const sideRealmId = this.realmTileMap.getRealmByRegion(sideRegionId)
+            if (realmId !== sideRealmId) {
+                return this._buildBoundary(realmId, sideRealmId)
+            }
+        }
+    }
+
+    _buildBoundary(realmId, sideRealmId) {
+        const dirToSide = this.realmTileMap.getRealmDirection(realmId, sideRealmId)
+        const dirFromSide = this.realmTileMap.getRealmDirection(sideRealmId, realmId)
+        const plateDir = this.plateModel.getDirection(realmId)
+        const sidePlateDir = this.plateModel.getDirection(sideRealmId)
+        const dotTo = Direction.dotProduct(plateDir, dirToSide)
+        const dotFrom = Direction.dotProduct(sidePlateDir, dirFromSide)
+        return this.boundaryTable.get(realmId, sideRealmId, dotTo, dotFrom)
     }
 
     getBoundaries() {
-        return this.boundaryModel.getBoundaries()
+        return this.#boundaries
     }
 
     setBoundaryByRegion(regionId, boundaryId) {
@@ -30,7 +66,18 @@ export class TectonicsModel {
 
     getBoundaryName(regionId) {
         const boundaryId = this.#regionBoundaryMap.get(regionId)
-        return this.boundaryModel.getName(boundaryId)
+        return this.#boundaryName[boundaryId]
+    }
+
+    getLandformByLevel(id, level) {
+        let name
+        const landscape = this.#boundaryLandscape[id]
+        for(let i=0; i<landscape.length; i++) {
+            name = landscape[i]
+            if (level <= i)
+                break
+        }
+        return Landform.get(name)
     }
 
     hasLandform(regionId) {
@@ -49,7 +96,7 @@ export class TectonicsModel {
 
 class BoundaryFloodFill extends GenericFloodFill {
     setValue(fillId, regionId, level) {
-        const landform = this.model.boundaryModel.getLandform(fillId, level)
+        const landform = this.model.getLandformByLevel(fillId, level)
         this.model.setLandform(regionId, landform)
         this.model.setBoundaryByRegion(regionId, fillId)
     }
@@ -65,8 +112,8 @@ class BoundaryFloodFill extends GenericFloodFill {
 
 
 class BoundaryMultiFill extends GenericMultiFill {
-    constructor(model) {
-        super(model.borderRegions, model, BoundaryFloodFill)
+    constructor(model, borderRegions) {
+        super(borderRegions, model, BoundaryFloodFill)
     }
 
     getChance(fillId, regionId) {
--- src/model/tilemap/world/geology/tectonics/table.js
+++ src/model/tilemap/world/geology/tectonics/table.js
@@ -1,5 +1,63 @@
+const PLATE_CONTINENTAL = 0
+const PLATE_OCEANIC = 100
+const DIR_CONVERGE = 1
+const DIR_TRANSFORM = 4
+const DIR_DIVERGE = 16
+const IDMAP = {
+    L: PLATE_CONTINENTAL,
+    W: PLATE_OCEANIC,
+    C: DIR_CONVERGE,
+    D: DIR_DIVERGE,
+    T: DIR_TRANSFORM,
+}
 
-export const BOUNDARY_TABLE = [
+
+export class BoundaryTable {
+    /*
+        Reads the boundary table and translates to manageable data.
+        Converts boundary code like 'LLCT' to its numeric id, summing
+        each character value.
+    */
+
+    #codeTable = new Map() // maps numeric id to boundary config
+
+    constructor(plateModel) {
+        this._plateModel = plateModel
+        BOUNDARY_TABLE.map(row => {
+            const key = Array.from(row.key)
+            const id = key.map(ch => IDMAP[ch]).reduce((a, b) => a + b, 0)
+            this.#codeTable.set(id, {...row, id})
+        })
+    }
+
+    get(realmId, sideRealmId, dotTo, dotFrom) {
+        const isPlateOceanic = this._plateModel.isOceanic(realmId)
+        const isSidePlateOceanic = this._plateModel.isOceanic(sideRealmId)
+        const type1 = isPlateOceanic ? PLATE_OCEANIC : PLATE_CONTINENTAL
+        const type2 = isSidePlateOceanic ? PLATE_OCEANIC : PLATE_CONTINENTAL
+        const dir = this._parseDir(dotTo) + this._parseDir(dotFrom)
+        const spec = this.#codeTable.get(type1 + type2 + dir)
+        const landscape = this._getLandscape(spec, realmId, sideRealmId)
+        return [spec.name, landscape]
+    }
+
+    _parseDir(dir) {
+        if (dir === 0) return DIR_TRANSFORM
+        return dir > 0 ? DIR_CONVERGE : DIR_DIVERGE
+    }
+
+    _getLandscape(spec, realmId, sideRealmId) {
+        const first = spec.data[0]
+        const second = spec.data.length === 1 ? first : spec.data[1]
+        const realmWeight = this._plateModel.getWeight(realmId)
+        const neighborRealmWeight = this._plateModel.getWeight(sideRealmId)
+        const data = realmWeight > neighborRealmWeight ? first : second
+        return data.landscape
+    }
+}
+
+
+const BOUNDARY_TABLE = [
     // ==============================================
     // CONTINENTAL-CONTINENTAL
     // ==============================================
