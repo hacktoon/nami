b968b468ac2d5738048a086454bc61ff9cc7b635
Karlisson Bezerra <macedobz@gmail.com>
2022-01-21 17:46:45 -0300

    Use fill context for all attributes

--- src/model/tilemap/world/geology/tecto-norealm/province/index.js
+++ src/model/tilemap/world/geology/tecto-norealm/province/index.js
@@ -14,47 +14,49 @@ export class ProvinceModel {
     #provinceMatrix
     #levelMatrix
     #deformationMatrix
-    #provinceIdList = []
-    #borderPointSet = new PointSet()
+    #borderPointSet
     #provinceMap = new Map()
-    #provinceMaxLevelMap = new Map()
+    #provinceMaxLevelMap
 
     constructor(regionTileMap, plateModel) {
-        const {width, height} = regionTileMap
         const boundaryModel = new BoundaryModel(regionTileMap, plateModel)
-
-        this.#levelMatrix = new Matrix(width, height)
-        this.#provinceMatrix = this._buildProvinceMatrix(
-            regionTileMap, boundaryModel,
-        )
+        const data = this._buildData(regionTileMap, boundaryModel)
+        this.#provinceMatrix = data.provinceMatrix
+        this.#levelMatrix = data.levelMatrix
+        this.#borderPointSet = data.borderPointSet
+        this.#provinceMaxLevelMap = data.provinceMaxLevelMap
         this.#deformationMatrix = this._buildDeformationMatrix(
             regionTileMap, boundaryModel,
         )
     }
 
-    _buildProvinceMatrix(regionTileMap, boundaryModel) {
+    _buildData(regionTileMap, boundaryModel) {
         const {width, height} = regionTileMap
         const originPoints = []
-        // builds the province matrix while reading the region border points
-        // used as fill origins, mapping the array index to its boundary types
-
-        const matrix = new Matrix(width, height, point => {
+        const provinceIdList = []
+        const borderPointSet = new PointSet()
+        const provinceMaxLevelMap = new Map()
+        const levelMatrix = new Matrix(width, height)
+        const provinceMatrix = new Matrix(width, height, point => {
             const borderRegions = regionTileMap.getBorderRegions(point)
             if (borderRegions.length > 0) {  // is a border point?
                 const region = regionTileMap.getRegion(point)
                 const sideRegion = borderRegions[0]
                 const province = boundaryModel.getProvince(region, sideRegion)
                 this.#provinceMap.set(province.id, province)
+                provinceMaxLevelMap.set(province.id, 0)
                 // these are like maps and use the index as key
-                this.#provinceIdList.push(province.id)
-                this.#provinceMaxLevelMap.set(province.id, 0)
+                provinceIdList.push(province.id)
                 originPoints.push(point)
             }
             return EMPTY
         })
-        const mapFill = new ProvinceConcurrentFill(originPoints, matrix, this)
+        const mapFill = new ProvinceConcurrentFill(originPoints, {
+            provinceMatrix, levelMatrix, provinceIdList, borderPointSet,
+            provinceMaxLevelMap
+        })
         mapFill.fill()
-        return matrix
+        return {provinceMatrix, levelMatrix, borderPointSet, provinceMaxLevelMap}
     }
 
     _buildDeformationMatrix(regionTileMap, boundaryModel) {
@@ -88,58 +90,41 @@ export class ProvinceModel {
     isProvinceBorder(point) {
         return this.#borderPointSet.has(point)
     }
-
-    // TODO: remove these fill methods, move to builder
-    _setFillValue(matrix, index, point, level) {
-        const id = this.#provinceIdList[index]
-        matrix.set(point, id)
-        this.#levelMatrix.set(point, level)
-        // updates max level to use on % of deformation calc
-        if (level > this.#provinceMaxLevelMap.get(id)) {
-            this.#provinceMaxLevelMap.set(id, level)
-        }
-    }
-
-    _setProvinceBorder(point) {
-        this.#borderPointSet.add(point)
-    }
-
-    _isFillEmpty(matrix, point) {
-        return matrix.get(point) === EMPTY
-    }
 }
 
 
 class ProvinceConcurrentFill extends ConcurrentFill {
-    constructor(origins, matrix, model) {
-        super(origins, ProvinceFloodFill, {matrix, model})
+    constructor(origins, context) {
+        super(origins, ProvinceFloodFill, context)
     }
     getChance(id, origin) { return .5 }
-    getGrowth(id, origin) { return 2 }
+    getGrowth(id, origin) { return 1 }
 }
 
 
 class ProvinceFloodFill extends ConcurrentFillUnit {
     setValue(index, point, level) {
-        const matrix = this._getContext('matrix')
-        const model = this._getContext('model')
-        model._setFillValue(matrix, index, point, level)
+        const provinceMaxLevelMap = this._getContext('provinceMaxLevelMap')
+        const id = this._getContext('provinceIdList')[index]
+        this._getContext('provinceMatrix').set(point, id)
+        this._getContext('levelMatrix').set(point, level)
+        // updates max level to use on % of deformation calc
+        if (level > provinceMaxLevelMap.get(id)) {
+            provinceMaxLevelMap.set(id, level)
+        }
     }
 
     isEmpty(point) {
-        const matrix = this._getContext('matrix')
-        const model = this._getContext('model')
-        return model._isFillEmpty(matrix, point)
+        return this._getContext('provinceMatrix').get(point) === EMPTY
     }
 
     checkNeighbor(index, sidePoint, centerPoint) {
         if (this.isEmpty(sidePoint)) return
-        const matrix = this._getContext('matrix')
-        const model = this._getContext('model')
-        const provinceId = matrix.get(centerPoint)
-        const sideProvinceId = matrix.get(sidePoint)
+        const provinceMatrix = this._getContext('provinceMatrix')
+        const provinceId = provinceMatrix.get(centerPoint)
+        const sideProvinceId = provinceMatrix.get(sidePoint)
         if (provinceId !== sideProvinceId) {
-            model._setProvinceBorder(centerPoint)
+            this._getContext('borderPointSet').add(centerPoint)
         }
     }
 
