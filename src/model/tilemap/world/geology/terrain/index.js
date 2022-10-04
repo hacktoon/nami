8ff3b64d123d9e93f0334aae0ad80a8e94f28eb5
Karlisson Bezerra <macedobz@gmail.com>
2022-10-03 22:04:26 -0300

    Move landmass generation to terrain model

--- src/model/tilemap/world/geology/terrain/index.js
+++ src/model/tilemap/world/geology/terrain/index.js
@@ -1,23 +1,27 @@
 import { Matrix } from '/src/lib/matrix'
 import { Point } from '/src/lib/point'
 import { PointSet } from '/src/lib/point/set'
+import { PairMap } from '/src/lib/map'
+import { ScanlineFill8 } from '/src/lib/floodfill/scanline'
 
 import { NoiseMapSet } from './noise'
-import { WaterSurfaceMap } from './surface'
+import { LandmassMap } from './surface'
 import { LAYERS, BASE_RATIO, BASE_NOISE, Terrain } from './schema'
+
+const MINIMUN_OCEAN_RATIO = 1  // 1%
 
 
 export class TerrainModel {
     #erosionPoints
     #borderPoints
     #terrainMap
-    #waterSurfaceMap
+    #landmassMap
 
     #buildLayer(props) {
         const newPoints = []
         const layerType = Terrain.isLand(props.layer.terrain) ? 'land' : 'water'
         const noiseMap = props.noiseMapSet.get(props.layer.noise)
-        props.pointQueue[layerType].forEach(point => {
+        props.points[layerType].forEach(point => {
             const noise = noiseMap.getNoise(point)
             if (this.#borderPoints.has(point) || noise < props.layer.ratio)
                 return
@@ -34,7 +38,7 @@ export class TerrainModel {
                 }
             }
         })
-        props.pointQueue[layerType] = newPoints
+        props.points[layerType] = newPoints
     }
 
     #buildBaseLayer(props) {
@@ -47,7 +51,7 @@ export class TerrainModel {
         return Matrix.fromRect(outlineNoiseMap.rect, point => {
             const layerType = getLayerType(point)
             const terrain = typeMap[layerType]
-            props.pointQueue[layerType].push(point)
+            props.points[layerType].push(point)
             // detect borders
             for (let sidePoint of Point.adjacents(point)) {
                 const sideTerrain = typeMap[getLayerType(sidePoint)]
@@ -67,19 +71,54 @@ export class TerrainModel {
             props.erosionPoints.get(terrain).push(point)
     }
 
+    #buildLandmassLayer(waterPoints) {
+        let landmassId = 1
+        const props = {
+            waterPointMap: new PairMap(),
+            bodies: new Map(),
+            oceans: new Set(),
+        }
+        const landmass = new LandmassMap(props)
+        for (let point of waterPoints) {
+            const status = this.#detectLandmass(props, landmassId, point)
+            landmassId += status ? 1 : 0
+        }
+        return landmass
+    }
+
+    #detectLandmass(props, landmassId, startPoint) {
+        let area = 0
+        const canFill = point => {
+            const isWater = Terrain.isWater(this.#terrainMap.get(point))
+            return isWater && ! props.waterPointMap.has(...point)
+        }
+        const onFill = point => {
+            props.waterPointMap.set(...point, landmassId)
+            area++
+        }
+        const filterPoint = point => this.#terrainMap.rect.wrap(point)
+        if (canFill(startPoint)) {
+            new ScanlineFill8(startPoint, {canFill, filterPoint, onFill}).fill()
+            props.bodies.set(landmassId, area)
+            const ratio = Math.round((area * 100) / this.#terrainMap.area)
+            if (ratio >= MINIMUN_OCEAN_RATIO) {
+                props.oceans.add(landmassId)
+            }
+            return true
+        }
+        return false
+    }
+
     constructor(rect, seed) {
-        const pointQueue = {water: [], land: []}
+        const points = {water: [], land: []}
         const noiseMapSet = new NoiseMapSet(rect, seed)
         const erosionPoints = new Map(
             Terrain.landTypes().map(terrain => [terrain.id, []])
         )
-        const props = {pointQueue, noiseMapSet, erosionPoints}
+        const props = {points, noiseMapSet, erosionPoints}
         this.#borderPoints = new PointSet()
         this.#terrainMap = this.#buildBaseLayer(props)
-        this.#waterSurfaceMap = new WaterSurfaceMap(
-            this.#terrainMap,
-            pointQueue.water
-        )
+        this.#landmassMap = this.#buildLandmassLayer(points.water)
         for (let layer of LAYERS) {
             this.#buildLayer({...props, layer})
         }
@@ -97,7 +136,8 @@ export class TerrainModel {
     }
 
     isOcean(point) {
-        return this.#waterSurfaceMap.isOcean(point)
+        const wrappedPoint = this.#terrainMap.rect.wrap(point)
+        return this.#landmassMap.isOcean(wrappedPoint)
     }
 
     getErosionPoints() {
--- src/model/tilemap/world/geology/terrain/surface.js
+++ src/model/tilemap/world/geology/terrain/surface.js
@@ -6,44 +6,16 @@ import { Terrain } from './schema'
 const MINIMUN_OCEAN_RATIO = 1  // 1%
 
 
-export class WaterSurfaceMap {
-    constructor(terrainMap, waterPoints) {
-        this.terrainMap = terrainMap
-        this.currentID = 1
-        this.filled = new PairMap()
-        this.bodies = new Map()
-        this.oceans = new Set()
-        for (let point of waterPoints){
-            this.#detectWaterbody(point)
-        }
+export class LandmassMap {
+    constructor({waterPointMap, bodies, oceans}) {
+        this.waterPointMap = waterPointMap
+        this.bodies = bodies
+        this.oceans = oceans
     }
 
-    #detectWaterbody(startPoint) {
-        let area = 0
-        const canFill = point => {
-            const isWater = Terrain.isWater(this.terrainMap.get(point))
-            return isWater && ! this.filled.has(...point)
-        }
-        const onFill = point => {
-            this.filled.set(...point, this.currentID)
-            area++
-        }
-        const filterPoint = point => this.terrainMap.rect.wrap(point)
-        if (canFill(startPoint)) {
-            new ScanlineFill8(startPoint, {canFill, filterPoint, onFill}).fill()
-            const id = this.currentID++
-            this.bodies.set(id, area)
-            const ratio = Math.round((area * 100) / this.terrainMap.area)
-            if (ratio >= MINIMUN_OCEAN_RATIO) {
-                this.oceans.add(id)
-            }
-        }
-    }
-
-    isOcean(rawPoint) {
-        const point = this.terrainMap.rect.wrap(rawPoint)
-        if (this.filled.has(...point)) {
-            const id = this.filled.get(...point)
+    isOcean(point) {
+        if (this.waterPointMap.has(...point)) {
+            const id = this.waterPointMap.get(...point)
             return this.oceans.has(id)
         }
         return false
