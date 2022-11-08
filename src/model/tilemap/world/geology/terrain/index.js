b2691191e12f3063d4227366bfffe8b18e9561e7
Karlisson Bezerra <macedobz@gmail.com>
2022-11-07 23:15:11 -0300

    Merge fill

--- src/model/tilemap/world/geology/terrain/index.js
+++ src/model/tilemap/world/geology/terrain/index.js
@@ -1,14 +1,22 @@
+import { ConcurrentFill, ConcurrentFillUnit } from '/src/lib/floodfill/concurrent'
+import { Direction } from '/src/lib/direction'
 import { Matrix } from '/src/lib/matrix'
 import { Point } from '/src/lib/point'
 import { PairMap } from '/src/lib/map'
 import { PointSet } from '/src/lib/point/set'
 
-import { TerrainConcurrentFill } from './fill'
 import { LAND_LAYERS, WATER_LAYERS, Terrain } from '../data'
 
 
 const EMPTY = null
 
+const PHASES = [
+    Terrain.BASIN,
+    // Terrain.PLAIN,
+    // Terrain.PLATEAU,
+    // Terrain.MOUNTAIN,
+    // Terrain.PEAK,
+]
 
 export class TerrainLayer {
     #landBorders = new PointSet()
@@ -33,8 +41,7 @@ export class TerrainLayer {
         const context = {
             noiseLayer: this.#noiseLayer,
             surfaceLayer: this.#surfaceLayer,
-            waterBorders: this.#waterBorders,
-            landBorders: this.#landBorders,
+            borders: this.#landBorders,
             basinMap: this.#basinMap,
             flowMap: this.#flowMap,
             matrix: matrix,
@@ -71,5 +78,71 @@ export class TerrainLayer {
 
     isWaterBorder(point) {
         return this.#waterBorders.has(point)
+    }
+}
+
+
+
+export class TerrainConcurrentFill extends ConcurrentFill {
+    constructor(origins, context) {
+        super(origins, TerrainFloodFill, context, PHASES)
+    }
+
+    getChance(ref, origin) { return 0 }
+    getGrowth(ref, origin) { return 1 }
+}
+
+
+class TerrainFloodFill extends ConcurrentFillUnit {
+    setValue(ref, point, level) {
+        const terrain = ref.fill.phase
+        const wrappedPoint = ref.context.matrix.rect.wrap(point)
+        // ref.context.basinMap.set(...wrappedPoint, ref.id)
+        ref.context.matrix.set(wrappedPoint, terrain)
+    }
+
+    isEmpty(ref, relativeSidePoint) {
+        const sidePoint = ref.context.matrix.rect.wrap(relativeSidePoint)
+        const noise = ref.noiseLayer.get(sidePoint)
+        const isLand = Terrain.isLand(sideTerrainId)
+        const isBorder = ref.context.borders.has(sidePoint)
+        const isEmpty = ref.context.matrix.get(sidePoint) === EMPTY
+        return isLand && isBorder && isEmpty
+    }
+
+    isPhaseEmpty(ref, sidePoint) {
+        return this.isEmpty(ref, sidePoint)
+    }
+
+    getNeighbors(ref, originPoint) {
+        return Point.adjacents(originPoint)
+    }
+
+    checkNeighbor(ref, sidePoint, centerPoint) {
+        const wSidePoint = ref.context.matrix.rect.wrap(sidePoint)
+        const wCenterPoint = ref.context.matrix.rect.wrap(centerPoint)
+        const sideTerrainId = ref.context.matrix.get(sidePoint)
+        const landBorders = ref.context.landBorders
+        const isSideWater = Terrain.isWater(sideTerrainId)
+        // detect river mouth
+        if (landBorders.has(wCenterPoint) && isSideWater) {
+            const directionId = this.getDirectionId(centerPoint, sidePoint)
+            ref.context.flowMap.set(...wCenterPoint, directionId)
+            return
+        }
+        if (ref.context.flowMap.has(...wSidePoint) || isSideWater) {
+            return
+        }
+
+        // set flow only on current or lower terrain layer
+        if (sideTerrainId <= ref.fill.phase + 1) {
+            const directionId = this.getDirectionId(sidePoint, centerPoint)
+            ref.context.flowMap.set(...wSidePoint, directionId)
+        }
+    }
+
+    getDirectionId(sourcePoint, targetPoint) {
+        const angle = Point.angle(sourcePoint, targetPoint)
+        return Direction.fromAngle(angle).id
     }
 }
