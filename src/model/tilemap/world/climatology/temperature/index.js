c00a1498c0ce89ea3131673d25f05fe560117069
Karlisson Bezerra <macedobz@gmail.com>
2023-01-04 00:19:24 -0300

    Refactor

--- src/model/tilemap/world/climatology/temperature/index.js
+++ src/model/tilemap/world/climatology/temperature/index.js
@@ -9,27 +9,16 @@ const TROPICAL_RATIO = .7
 // TODO: temp is dynamic, make noise offset and loop
 
 export class TemperatureLayer {
-    #offset
     #noiseLayer
     #reliefLayer
 
     constructor(rect, noiseLayer, reliefLayer) {
         this.#noiseLayer = noiseLayer
         this.#reliefLayer = reliefLayer
-        this.#offset = [
-            Math.floor(rect.width / 4),
-            Math.floor(rect.height / 4)
-        ]
     }
 
-    #detectType(point) {
+    #detectBaseType(point) {
         const featureNoise = this.#noiseLayer.getAtmos(point)
-
-        // TODO: decrease one temp if its a mountain
-        if (this.#reliefLayer.isMountain(point)) {
-            return Temperature.POLAR
-        }
-
         if (featureNoise > TEMPERATE_RATIO) {
             if (featureNoise > SUBTROPICAL_RATIO) {
                 if (featureNoise > TROPICAL_RATIO)
@@ -42,6 +31,11 @@ export class TemperatureLayer {
     }
 
     get(point) {
-        return this.#detectType(point)
+        const baseType = this.#detectBaseType(point)
+        // TODO: decrease one temp if its a mountain
+        if (this.#reliefLayer.isMountain(point)) {
+            return Temperature.POLAR
+        }
+        return baseType
     }
 }
