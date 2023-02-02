03a55808b77c3ebbe8d30bb0e3794dc9baab2e16
Karlisson Bezerra <macedobz@gmail.com>
2023-02-02 19:41:08 -0300

    Tweak biomes

--- src/model/tilemap/world/biology/biome/index.js
+++ src/model/tilemap/world/biology/biome/index.js
@@ -25,7 +25,10 @@ export class BiomeLayer {
                 return Biome.ICE_PLAIN
             }
             if (layers.relief.isTrench(point)) return Biome.TRENCH
-            if (layers.relief.isSea(point)) return Biome.SEA
+            if (layers.relief.isSea(point)) {
+                // TODO: add coral reef
+                return Biome.SEA
+            }
             return Biome.OCEAN
         }
 
@@ -40,24 +43,24 @@ export class BiomeLayer {
 
         if (temperature.isCold()) {
             if (rain.isHumid()) return Biome.TUNDRA
-            if (rain.isWet()) return Biome.TUNDRA
+            if (rain.isWet()) return Biome.TAIGA
             if (rain.isSeasonal()) return Biome.TAIGA
             if (rain.isDry()) return Biome.WOODLANDS
             if (rain.isArid()) return Biome.GRASSLANDS
         }
 
         if (temperature.isTemperate()) {
-            if (rain.isHumid()) return Biome.WOODLANDS
+            if (rain.isHumid()) return Biome.TAIGA
             if (rain.isWet()) return Biome.WOODLANDS
-            if (rain.isSeasonal()) return Biome.GRASSLANDS
+            if (rain.isSeasonal()) return Biome.WOODLANDS
             if (rain.isDry()) return Biome.GRASSLANDS
-            if (rain.isArid()) return Biome.SAVANNA
+            if (rain.isArid()) return Biome.GRASSLANDS
         }
 
         if (temperature.isWarm()) {
             if (rain.isHumid()) return Biome.JUNGLE
             if (rain.isWet()) return Biome.WOODLANDS
-            if (rain.isSeasonal()) return Biome.SAVANNA
+            if (rain.isSeasonal()) return Biome.GRASSLANDS
             if (rain.isDry()) return Biome.SAVANNA
             if (rain.isArid()) return Biome.DESERT
         }
@@ -67,9 +70,8 @@ export class BiomeLayer {
             if (rain.isWet()) return Biome.JUNGLE
             if (rain.isSeasonal()) return Biome.SAVANNA
             if (rain.isDry()) return Biome.DESERT
-            if (rain.isArid()) return Biome.DESERT
+            if (rain.isArid()) return Biome.WASTELAND
         }
-        return Biome.GRASSLANDS
     }
 
     get(point) {
