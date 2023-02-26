fb1a49ad1daa823794fd035c2f578aabf79ece04
Karlisson Bezerra <macedobz@gmail.com>
2023-02-26 19:36:06 -0300

    Refactor

--- src/model/tilemap/world/topology/index.js
+++ src/model/tilemap/world/topology/index.js
@@ -3,9 +3,6 @@ import { Random } from '/src/lib/random'
 import { Point } from '/src/lib/point'
 import { PointSet, PointArraySet } from '/src/lib/point/set'
 
-import { Temperature } from '../climatology/temperature/data'
-import { Place } from './data'
-
 
 const WATER_CITY_CHANCE = .005
 const CITY_RADIUS = 3
@@ -20,19 +17,20 @@ export class TopologyLayer {
     #realmCount
 
     constructor(rect, layers, realmCount) {
-        const candidateCityPoints = new PointArraySet()
+        const possibleCityPoints = new PointArraySet()
         this.#realmCount = realmCount
         Matrix.fromRect(rect, point => {
-            if (this.#isCandidateCity(layers, point)) {
-                candidateCityPoints.add(point)
+            const isPossibleCity = this.#isPossibleCity(layers, point)
+            if (isPossibleCity) {
+                possibleCityPoints.add(point)
             }
             // set matrix init value
             this.#placeMap.set(point, 1)
         })
-        this.#cityPoints = this.#buildCities(rect, candidateCityPoints)
+        this.#cityPoints = this.#buildCities(rect, possibleCityPoints)
     }
 
-    #isCandidateCity(layers, point) {
+    #isPossibleCity(layers, point) {
         const isLand = layers.surface.isLand(point)
         const isBorder = layers.relief.isBorder(point)
         const isPeak = layers.relief.isPeak(point)
@@ -43,14 +41,14 @@ export class TopologyLayer {
         return isWaterCity || isLandCity
     }
 
-    #buildCities(rect, candidateCityPoints) {
+    #buildCities(rect, possibleCityPoints) {
         const cityPoints = new PointSet()
         let realmId = this.#realmCount
-        while (candidateCityPoints.size > 0) {
-            const center = candidateCityPoints.random()
+        while (possibleCityPoints.size > 0) {
+            const center = possibleCityPoints.random()
             // remove candidate points around a city circle
             Point.insideCircle(center, CITY_RADIUS, point => {
-                candidateCityPoints.delete(rect.wrap(point))
+                possibleCityPoints.delete(rect.wrap(point))
             })
             cityPoints.add(center)
             if (realmId > 0)
@@ -58,11 +56,6 @@ export class TopologyLayer {
             realmId--
         }
         return cityPoints
-    }
-
-    #buildCity(rect, candidateCityPoints) {
-
-        return {}
     }
 
     isCity(point) {
