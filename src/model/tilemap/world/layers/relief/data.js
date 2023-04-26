c5e5e2a035af8b14fb751e7b4dab0ebbe0243373
Karlisson Bezerra <macedobz@gmail.com>
2023-04-26 20:55:20 -0300

    Update plain color

--- src/model/tilemap/world/layers/relief/data.js
+++ src/model/tilemap/world/layers/relief/data.js
@@ -23,7 +23,7 @@ export class Relief {
     static ABYSS = Spec.build({name: 'Abyss', color: '#185574'})
     static OCEAN = Spec.build({name: 'Ocean', color: '#216384'})
     static PLATFORM = Spec.build({name: 'Platform', color: '#2878a0'})
-    static PLAIN = Spec.build({name: 'Plain', color: '#77AA77'})
+    static PLAIN = Spec.build({name: 'Plain', color: '#66b169'})
     static HILL = Spec.build({name: 'Hill', color: '#88BB88'})
     static PLATEAU = Spec.build({name: 'Plateau', color: '#95af8b'})
     static MOUNTAIN = Spec.build({name: 'Mountain', color: '#badfba'})
