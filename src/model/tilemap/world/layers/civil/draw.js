3a17f92018fc9ed5a8c9649f72174fce97df7920
Karlisson Bezerra <macedobz@gmail.com>
2024-01-14 00:53:25 -0300

    Refactor draw map

--- src/model/tilemap/world/layers/civil/draw.js
+++ src/model/tilemap/world/layers/civil/draw.js
@@ -1,48 +1,5 @@
-import { Point } from '/src/lib/point'
 import { Color } from '/src/lib/color'
 import { drawIcon } from '/src/ui/tilemap/draw'
-
-
-export function drawTown(props) {
-    const template = [
-        [0, 0, 0, 1, 1, 0, 0, 0],
-        [0, 0, 1, 4, 3, 1, 0, 0],
-        [0, 1, 4, 4, 3, 3, 1, 0],
-        [1, 4, 4, 4, 3, 3, 3, 1],
-        [1, 4, 4, 1, 1, 3, 3, 1],
-        [1, 4, 1, 2, 2, 1, 3, 1],
-        [1, 1, 2, 1, 1, 2, 1, 1],
-        [0, 1, 1, 1, 1, 1, 1, 0],
-    ]
-    const colorMap = {
-        1: Color.BLACK,
-        2: Color.fromHex('CCC'),
-        3: Color.fromHex('fd004d'),
-        4: Color.DARKRED,
-    }
-    drawIcon(props, template, colorMap)
-}
-
-
-export function drawVillage(props) {
-    const template = [
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 1, 0, 0],
-        [0, 1, 4, 4, 3, 3, 1, 0],
-        [1, 4, 4, 1, 1, 3, 3, 1],
-        [1, 4, 1, 2, 2, 1, 3, 1],
-        [1, 1, 2, 1, 2, 2, 1, 1],
-        [0, 1, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-    ]
-    const colorMap = {
-        1: Color.BLACK,
-        2: Color.fromHex('#d6b498'),
-        3: Color.fromHex('#d47f47'),
-        4: Color.fromHex('#b36028'),
-    }
-    drawIcon(props, template, colorMap)
-}
 
 
 export function drawCapital(props) {
@@ -65,20 +22,43 @@ export function drawCapital(props) {
 }
 
 
-export function drawDungeon(baseProps) {
+export function drawTown(props) {
     const template = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0, 0, 0],
+        [0, 0, 1, 4, 3, 1, 0, 0],
+        [0, 1, 4, 4, 3, 3, 1, 0],
+        [1, 4, 4, 1, 1, 3, 3, 1],
+        [1, 4, 1, 2, 2, 1, 3, 1],
+        [1, 1, 2, 1, 1, 2, 1, 1],
+        [0, 1, 2, 1, 1, 2, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
     ]
     const colorMap = {
-        1: Color.BROWN,
-        2: Color.BLACK,
+        1: Color.BLACK,
+        2: Color.fromHex('CCC'),
+        3: Color.fromHex('fd004d'),
+        4: Color.DARKRED,
     }
-    const midSize = Math.round(baseProps.tileSize / 2)
-    const midPoint = Point.plus(baseProps.canvasPoint, [midSize, 0])
-    const props = {...baseProps, canvasPoint: midPoint}
+    drawIcon(props, template, colorMap)
+}
+
+
+export function drawVillage(props) {
+    const template = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 4, 4, 3, 1, 0, 0],
+        [1, 4, 4, 1, 3, 3, 1, 0],
+        [1, 4, 1, 2, 1, 3, 1, 0],
+        [1, 1, 2, 1, 2, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    const colorMap = {
+        1: Color.BLACK,
+        2: Color.fromHex('#d6b498'),
+        3: Color.fromHex('#d47f47'),
+        4: Color.fromHex('#b36028'),
+    }
     drawIcon(props, template, colorMap)
 }
