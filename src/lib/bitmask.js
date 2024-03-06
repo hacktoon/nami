5e606d678be0419fcc1204bd8aaef22093d29599
Karlisson Bezerra <macedobz@gmail.com>
2024-03-05 23:27:48 -0300

    Add direction bitmask

--- src/lib/bitmask.js
+++ src/lib/bitmask.js
@@ -1,3 +1,6 @@
+import { Direction } from '/src/lib/direction'
+
+
 export class BitMask {
     #value
 
@@ -23,5 +26,47 @@ export class BitMask {
 
     has(flag) {
         return flag === (this.#value & flag)
+    }
+}
+
+
+export class DirectionBitMask {
+    #bitmask
+    // bitmask value => point in matrix 3x3
+    /*
+        1(N)
+    2(W)         8 (E)
+        16(S)
+    */
+    // detect matrix in source file
+    #map = new Map([
+        [Direction.NORTH.id, 1],
+        [Direction.WEST.id, 2],
+        [Direction.EAST.id, 8],
+        [Direction.SOUTH.id, 16],
+    ])
+
+    constructor(value=0) {
+        this.#bitmask = new BitMask(value)
+    }
+
+    get value() {
+        return this.#bitmask.value
+    }
+
+    set(flag) {
+        this.#bitmask.set(flag)
+    }
+
+    unset(flag) {
+        this.#bitmask.unset(flag)
+    }
+
+    toggle(flag) {
+        this.#bitmask.toggle(flag)
+    }
+
+    has(flag) {
+        return this.#bitmask.has(flag)
     }
 }
